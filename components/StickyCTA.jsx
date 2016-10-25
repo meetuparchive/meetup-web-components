1c3ced5bb4d87e490436b9a53a18eb224720662d
rxb <boenigk@gmail.com>
2016-10-25 12:33:29 -0400

    rearranging

--- components/StickyCTA.jsx
+++ components/StickyCTA.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 
-import { Section } from 'foundation-react/layoutUtils';
+import { Section } from './layoutUtils';
 
 /**
  * @module StickyCTA
