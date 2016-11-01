9564fcb2eeb2fc9d4fa8784561a6986d63b42a80
rxb <boenigk@gmail.com>
2016-11-01 18:36:30 -0400

    card

---
+++ src/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import cx from 'classnames';
+
+/**
+ * @module Card
+ */
+class Card extends React.Component {
+	render() {
+		const {
+			children,
+			className,
+			...other
+		} = this.props;
+
+		const classNames = cx(
+			'card',
+			className
+		);
+
+		return (
+			<div
+				className={classNames}
+				{...other}>
+					Hello from Card
+					{children}
+			</div>
+		);
+	}
+}
+
+Card.propTypes = {
+};
+
+export default Card;
