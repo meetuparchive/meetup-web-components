53a1b99717744d57112dc941bd9b5e0a788d0499
rxb <boenigk@gmail.com>
2016-11-02 14:16:40 -0400

    modal closing

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import cx from 'classnames';
-
+import { Link } from 'react-router';
+import Icon from './Icon';
 /**
  * @module Modal
  */
 class Modal extends React.Component {
+
+	static get contextTypes() {
+		return {
+			router: React.PropTypes.object,
+		};
+	}
+
+	constructor(props){
+		super(props);
+		this._handleOutClick = this._handleOutClick.bind(this);
+	}
+
+	_handleOutClick(event){
+		event.stopPropagation();
+		// TODO:
+		// detect if this should actually jump back in history instead of pushing forward
+		// click should drop you to the underlying layer, but that's not always where you came from
+		this.context.router.push(this.props.closeUrl);
+	}
+
 	render() {
 		const {
 			children,
@@ -21,11 +42,16 @@ class Modal extends React.Component {
 			<div
 				className={classNames}
 				{...other}>
-				<div className='overlayShim'>
+				<div className='overlayShim' onClick={this._handleOutClick}>
 					<div className='overlayShim-content inverted'>
 					</div>
 				</div>
 				<div className='view view--modalSnap' >
+					<div className='padding--all'>
+						<Link to={this.props.closeUrl}>
+							<Icon shape="close" size="s" />
+						</Link>
+					</div>
 					{children}
 				</div>
 			</div>
