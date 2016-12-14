605e270e96d8726797fc36def3dd2e55ffcc2521
mperrotti <mperrotti573@gmail.com>
2016-12-14 17:36:02 -0500

    corrected test, changed to export default instead of destructured

--- src/flexItem.test.jsx
+++ src/flexItem.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import { IntlProvider } from 'react-intl'
-import FlexItemContainer from './FlexItemContainer';
+import { IntlProvider } from 'react-intl';
+import FlexItem from './FlexItem';
 
 function intlRender(component) {
 	return TestUtils.renderIntoDocument(
@@ -12,10 +12,10 @@ function intlRender(component) {
 	);
 }
 
-describe('FlexItemContainer', function() {
+describe('FlexItem', function() {
 
 	it('exists', function() {
-		const flexItem = intlRender(<FlexItemContainer />);
+		const flexItem = intlRender(<FlexItem />);
 		const flexItemNode = ReactDOM.findDOMNode(flexItem);
 
 		expect(flexItemNode).not.toBeNull();
--- src/layoutUtils.js
+++ src/layoutUtils.js
@@ -1,6 +1,7 @@
 export {default as Bounds} from './Bounds';
 export {default as Chunk} from './Chunk';
-export {Flex, FlexItem} from './Flex';
+export {default as Flex} from './Flex';
+export {default as FlexItem} from './FlexItem';
 export {default as GridList} from './GridList';
 export {default as GridListItem} from './GridListItem';
 export {default as PageActions} from './PageActions';
