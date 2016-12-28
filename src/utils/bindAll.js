fc0735bf8015828891d5532408cb37104fe7b760
Adam Detrick <akdetrick@gmail.com>
2016-12-28 18:55:44 -0500

    fix css; add bindAll util

---
+++ src/utils/bindAll.js
@@ -0,0 +1,18 @@
+/**
+ * @function bindAll
+ * helper for binding prototype methods to class context
+ *
+ * for example, instead of doing this in a class `constructor`:
+ * `this.handleClick = this.handleClick.bind(this)`
+ *
+ * you can use `bindAll`:
+ * ```
+ * bindAll(this,
+ * 	handleClick,
+ * 	someOtherMethod,
+ * 	anotherMethod);
+ * ```
+ */
+export const bindAll = (context, ...names) => {
+	names.forEach(name => context[name] = context[name].bind(context));
+};
