28555a2351bdade334b00863ac864bedff86ae93
Adam Detrick <akdetrick@gmail.com>
2017-01-06 01:14:43 -0500

    rm unused icons from popover story

--- src/popover.story.jsx
+++ src/popover.story.jsx
@@ -4,7 +4,6 @@ import { decorateWithLocale } from './utils/decorators';
 import { Link } from 'react-router';
 import Popover from './Popover';
 import Button from './Button';
-import { TestIconSprite } from './utils/storyComponents';
 
 const logSelection = e => {
 	action(`Selected option ${e.target}`);
@@ -14,33 +13,27 @@ storiesOf('Popover', module)
 	.addDecorator(decorateWithLocale)
 	.add('Button trigger with Link options', () => {
 		return (
-			<div>
-				<TestIconSprite />
-				<Popover
-					trigger={
-						<Button>Open</Button>
-					}
-					options={[
-						<Link to='somepath/' onClick={logSelection}>First option</Link>,
-						<Link to='somepath/' onClick={logSelection}>Second option</Link>,
-						<Link to='somepath/' onClick={logSelection}>Third option</Link>,
-					]}
-				/>
-			</div>
+			<Popover
+				trigger={
+					<Button>Open</Button>
+				}
+				options={[
+					<Link to='somepath/' onClick={logSelection}>First option</Link>,
+					<Link to='somepath/' onClick={logSelection}>Second option</Link>,
+					<Link to='somepath/' onClick={logSelection}>Third option</Link>,
+				]}
+			/>
 		);
 	})
 	.add('DIV trigger with SPAN options', () => {
 		return (
-			<div>
-				<TestIconSprite />
-				<Popover
-					trigger={<div>Open</div>}
-					options={[
-						<span onClick={logSelection}>First option</span>,
-						<span to='somepath/' onClick={logSelection}>Second option</span>,
-						<span to='somepath/' onClick={logSelection}>Third option</span>,
-					]}
-				/>
-			</div>
+			<Popover
+				trigger={<div>Open</div>}
+				options={[
+					<span onClick={logSelection}>First option</span>,
+					<span to='somepath/' onClick={logSelection}>Second option</span>,
+					<span to='somepath/' onClick={logSelection}>Third option</span>,
+				]}
+			/>
 		);
 	});
