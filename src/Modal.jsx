4372382abe453b6f9adf6b70f5fc4398817f2579
mperrotti <mperrotti573@gmail.com>
2017-01-11 17:01:04 -0500

    fixed Modal merge conflicts

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,31 +1,5 @@
 import React from 'react';
 import cx from 'classnames';
-<<<<<<< HEAD
-import { Link } from 'react-router';
-import Icon from './Icon';
-/**
- * @module Modal
- */
-class Modal extends React.Component {
-
-	static get contextTypes() {
-		return {
-			router: React.PropTypes.object,
-		};
-	}
-
-	constructor(props){
-		super(props);
-		this._handleOutClick = this._handleOutClick.bind(this);
-	}
-
-	_handleOutClick(event){
-		event.stopPropagation();
-		// TODO:
-		// detect if this should actually jump back in history instead of pushing forward
-		// click should drop you to the underlying layer, but that's not always where you came from
-		this.context.router.push(this.props.closeUrl);
-=======
 import Icon from './Icon';
 import Button from './Button';
 
@@ -55,22 +29,10 @@ class Modal extends React.Component {
 		if (e.key === 'Escape') {
 			this.onDismiss(e);
 		}
->>>>>>> d6275df2c3cc39badacafd377316daf7f1a45f7d
 	}
 
 	render() {
 		const {
-<<<<<<< HEAD
-			children,
-			className,
-			fullScreen,
-			...other
-		} = this.props;
-
-		const classNames = cx(
-			'modal',
-			className
-=======
 			className,
 			children,
 			fullscreen,
@@ -96,33 +58,10 @@ class Modal extends React.Component {
 		const dismissButtonClasses = cx(
 			MODAL_CLOSE_BUTTON,
 			'border--none'
->>>>>>> d6275df2c3cc39badacafd377316daf7f1a45f7d
 		);
 
 		return (
 			<div
-<<<<<<< HEAD
-				className={classNames}
-				{...other}>
-				<div className='overlayShim' onClick={this._handleOutClick}>
-					<div className='overlayShim-content inverted'>
-					</div>
-				</div>
-				<div
-					className={cx(
-						'view',
-						{
-							'view--modalFull': fullScreen,
-							'view--modalSnap': !fullScreen
-						}
-					)}
-				>
-					<div className='modal-close padding--all'>
-						<Link to={this.props.closeUrl}>
-							<Icon shape='close' size='s' />
-						</Link>
-					</div>
-=======
 				role='dialog'
 				tabIndex='0'
 				onKeyDown={this.onKeyDown}
@@ -139,8 +78,6 @@ class Modal extends React.Component {
 							<Icon shape='cross' size='s' />
 						</Button>
 					</div>
-
->>>>>>> d6275df2c3cc39badacafd377316daf7f1a45f7d
 					{children}
 				</div>
 			</div>
@@ -149,16 +86,12 @@ class Modal extends React.Component {
 }
 
 Modal.propTypes = {
-<<<<<<< HEAD
-	full: React.PropTypes.bool
-=======
 	onDismiss: React.PropTypes.func.isRequired,
 	fullscreen: React.PropTypes.bool
 };
 
 Modal.defaultProps = {
 	fullscreen: false
->>>>>>> d6275df2c3cc39badacafd377316daf7f1a45f7d
 };
 
 export default Modal;
