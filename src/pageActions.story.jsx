a3e8281afa8c0c581ec28c0625b84233f714e0c1
mperrotti <mperrotti573@gmail.com>
2017-01-15 14:24:48 -0500

    added story for <3 actions in PageHead

--- src/pageActions.story.jsx
+++ src/pageActions.story.jsx
@@ -20,6 +20,18 @@ storiesOf('PageActions', module)
 			</PageActions>
 		</div>
 	))
+	.add('Row with less than 3 actions', () => (
+		<div style={{width: '100%', padding: '16px'}}>
+			<PageActions>
+				<PageAction>
+					<PageActionButton icon='magnifying-glass' label='Search' />
+				</PageAction>
+				<PageAction>
+					<PageActionButton icon='edit' label='Edit' />
+				</PageAction>
+			</PageActions>
+		</div>
+	))
 	.add('Column', () => (
 		<div style={{width: '100%'}}>
 			<PageActions direction='column'>
