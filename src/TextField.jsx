3cfe8cc46b5efdb5eb5823183f46ce7dc2765f6c
ivy <ivy@meetup.com>
2017-01-20 18:04:19 -0500

    controlled or uncontrolled?

--- src/TextField.jsx
+++ src/TextField.jsx
@@ -5,8 +5,29 @@ import cx from 'classnames';
  * @module TextField
  */
 class TextField extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { value: props.value };
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	// question es6 in react??
+	handleChange(e) {
+		console.log('changed', e.target.value);
+		this.setState({ value: e.target.value });
+	}
+
+	getValue() {
+		return this.state.value;
+	}
+
 	render() {
 		const {
+			elId,
+			name,
+			label,
+			placeholder,
 			children,
 			className,
 			...other
@@ -17,11 +38,20 @@ class TextField extends React.Component {
 			className
 		);
 
+		console.log('render', this.state.value);
+
 		return (
-			<div
-				className={classNames}
-				{...other}>
-					Hello from TextField
+			<div>
+				<label htmlFor={elId}>{label}</label>
+				<input
+					type='text'
+					id={elId}
+					name={name}
+					placeholder={placeholder}
+					value={this.getValue}
+					onChange={this.handleChange}
+					className={classNames}
+					{...other} />
 					{children}
 			</div>
 		);
@@ -29,6 +59,7 @@ class TextField extends React.Component {
 }
 
 TextField.propTypes = {
+	name: React.PropTypes.string.isRequired,
 };
 
 export default TextField;
--- src/textField.story.jsx
+++ src/textField.story.jsx
@@ -3,5 +3,17 @@ import React from 'react';
 import TextField from './TextField';
 import { storiesOf } from '@kadira/storybook';
 
+
 storiesOf('TextField', module)
-	.add('default', () => <TextField />)
+	.add('default', () => <TextField
+		label='Your name'
+		elId='fullname'
+		name='name'
+		value='Q tip'
+		placeholder='Not your email...' />)
+	.add('has a value', () => <TextField
+		label='Your name'
+		elId='fullname'
+		name='name'
+		value='Phife Dawg'
+		placeholder='Not your email...' />);
