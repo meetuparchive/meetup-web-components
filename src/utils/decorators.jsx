0ebfc18a5fd9778987fed0b9be1595cdc9405833
Lori Hutchek <Lori.Hutchek@akqa.com>
2017-01-23 13:03:58 -0500

    adding swarm icons library

--- src/utils/decorators.jsx
+++ src/utils/decorators.jsx
@@ -3,8 +3,18 @@ import { IntlProvider } from 'react-intl';
 
 export const decorateWithLocale = story => {
 	const locale = 'en-US';
+
+	/*
+	 * -- Inline SVG icon sprite --
+	 *
+	 * raw SVG sprite from `swarm-icons`
+	 */
+	const iconSpriteStyle = { display: 'none' };
+	const iconSprite = require('raw-loader!swarm-icons/dist/sprite/sprite.inc');
+
 	return (
 		<IntlProvider locale={locale}>
+			<div style={iconSpriteStyle} dangerouslySetInnerHTML={{__html: iconSprite}} />
 			{story()}
 		</IntlProvider>
 	);
