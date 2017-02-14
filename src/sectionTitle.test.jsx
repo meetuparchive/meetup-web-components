aab771265f3885549d3a9cf1131b7e3cd9c653c4
Adam Detrick <akdetrick@gmail.com>
2017-02-14 16:01:17 -0500

    fix test; use virual DOM to check class assignment

--- src/sectionTitle.test.jsx
+++ src/sectionTitle.test.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
 import SectionTitle, { SECTIONTITLE_CLASS } from './SectionTitle';
 
 describe('SectionTitle', function() {
-	let sectionTitle,
-		titleNode;
+	let component;
 
 	beforeEach(() => {
-		sectionTitle = TestUtils.renderIntoDocument(<SectionTitle />);
-		titleNode = ReactDOM.findDOMNode(sectionTitle);
+		component = TestUtils.renderIntoDocument(<SectionTitle />);
 	});
 	afterEach(() => {
-		sectionTitle = null;
-		titleNode = null;
-	});
-
-	it('exists', function() {
-		expect(titleNode).not.toBeNull();
+		component = null;
 	});
 
 	it(`check that default component has '${SECTIONTITLE_CLASS}' class`, function() {
-		expect(titleNode.classList).toContain(SECTIONTITLE_CLASS);
+		const topNode = TestUtils.scryRenderedDOMComponentsWithClass(component, SECTIONTITLE_CLASS);
+		expect(topNode.length).toBe(1);
 	});
 
 });
