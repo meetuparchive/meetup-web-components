64b6f81c36ca5a36feded70940935aeb8a259c6c
ivy <ivy@meetup.com>
2017-02-17 16:43:46 -0500

    getting things going with datepicker and format

--- src/DateTimePicker.jsx
+++ src/DateTimePicker.jsx
@@ -10,48 +10,62 @@ class DateTimePicker extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			value: props.value || '',
 			isSupported: false
 		};
-	}
-
-	componentWillReceiveProps(props) {
-		if (props.value) {
-			this.flatpickr && this.flatpickr.setDate(props.value);
-		}
+		this.onChange = this.onChange.bind(this);
 	}
 
 	// test for browser support
 	componentWillMount() {
 		const input = document.createElement('input'),
-			testValue = 'notadate';
+			invalidValue = 'notadate';
 
 		input.setAttribute('type', 'datetime-local');
-		input.setAttribute('value', testValue);
+		input.setAttribute('value', invalidValue);
+
+		// false to view storybook in chrome since chrome supports
+		const isValueDenied = (false && input.value !== invalidValue);
 
 		// some browsers (as Android stock browsers) pretend they support
 		// certain input types, so set the value and see
-		this.setState({ isSupported: input.value !== testValue });
+		this.setState({ isSupported: isValueDenied });
 	}
 
-	// init the js datetime component if there is no native support
+	// init the js datetime component
+	// if there is no native support
+
 	componentDidMount() {
 		if (!this.state.isSupported) {
 			const options = {
-				...this.props.options,
-				onChange: this.props.onChange
+				...this.props.datepickerOptions,
+				onChange: this.onChange,
+				altInput: true,
+				altFormat: 'M d, Y h:i K' // TODO localize
 			};
 			this.flatpickr = new Flatpickr(this.node, options);
 		}
+	}
+
+	componentWillReceiveProps(props) {
+		this.flatpickr && this.flatpickr.setDate(this.state.value);
 	}
 
 	componentWillUnmount() {
 		this.flatpickr && this.flatpickr.destroy();
 	}
 
+	// conforms with flatpickr onChange args
+	onChange(selectedDates, dateStr, instance) {
+		const newValue = (this.flatpickr) ? selectedDates[0] : selectedDates.target.value;
+		this.setState({ value: newValue });
+	}
+
 	render() {
 		const {
-			onChange, // eslint-disable-line no-unused-vars
-			options,  // eslint-disable-line no-unused-vars
+			datepickerOptions,  // eslint-disable-line no-unused-vars
+			id,
+			label,
 			value,
 			className,
 			...other
@@ -62,12 +76,23 @@ class DateTimePicker extends React.Component {
 			className
 		);
 
+		// if its supported we control the value through state and use onChange
+		// if not, we provide a default value and let flatpickr handle it
+		const onChangeProp = this.state.isSupported ? this.onChange : null;
+		const valueProp = this.state.isSupported ? { value: this.state.value } : { defaultValue: value };
+
 		return (
-			<input
-				value={value}
-				className={classNames}
-				ref={ node => this.node = node }
-				{...other} />
+			<div>
+				<label htmlFor={id}>{label}</label>
+				<input
+					id={id}
+					type={this.state.isSupported ? 'datetime-local' : 'text'}
+					className={classNames}
+					ref={ node => this.node = node }
+					{...valueProp}
+					onChange={onChangeProp}
+					{...other} />
+			</div>
 		);
 	}
 }
--- src/dateTimePicker.story.jsx
+++ src/dateTimePicker.story.jsx
@@ -3,4 +3,17 @@ import DateTimePicker from './DateTimePicker';
 import { storiesOf } from '@kadira/storybook';
 
 storiesOf('DateTimePicker', module)
-	.add('default', () => <DateTimePicker value={1477697199863} />);
+	.add('default', () => {
+		const opts = {
+			allowInput: true,
+			enableTime: true,
+			minDate: Date.now()
+		};
+		return (<div className='span--50'>
+			<DateTimePicker
+				label='Start at'
+				value='2017-02-18T14:30'
+				datepickerOptions={opts}
+			/>
+		</div>);
+	});
