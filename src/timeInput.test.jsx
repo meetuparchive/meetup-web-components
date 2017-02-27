bc5c84fa87c249f94bd2b51ef815971c94dc6d38
ivy <ivy@meetup.com>
2017-02-27 15:54:32 -0500

    setstate and onchange tests

--- src/timeInput.test.jsx
+++ src/timeInput.test.jsx
@@ -8,12 +8,18 @@ describe('TimeInput', function() {
 		timeInputEl;
 
 	const timeValue = '22:00',
+		newTime = '23:00',
 		callbackSpy = jasmine.createSpy('foo');
 
 	beforeEach(() => {
 		timeInputComponent = TestUtils.renderIntoDocument(
 			<TimeInput name='time' value={timeValue} callback={callbackSpy} />);
 		timeInputEl = TestUtils.findRenderedDOMComponentWithTag(timeInputComponent, 'input');
+	});
+
+	afterEach(() => {
+		timeInputComponent = null;
+		timeInputEl = null;
 	});
 
 	it('exists', function() {
@@ -28,10 +34,21 @@ describe('TimeInput', function() {
 		expect(timeInputComponent.state).toEqual({ value: timeValue });
 	});
 
+	it('calls onChange and sets state when value is changed', function() {
+		const onChangeSpy = spyOn(TimeInput.prototype, 'onChange').and.callThrough();
+		const setStateSpy = spyOn(TimeInput.prototype, 'setState');
+		timeInputComponent = TestUtils.renderIntoDocument(
+			<TimeInput name='time' value={timeValue} callback={callbackSpy} />);
+		timeInputEl = TestUtils.findRenderedDOMComponentWithTag(timeInputComponent, 'input');
+
+		TestUtils.Simulate.change(timeInputEl, { target: { value: newTime } });
+		expect(onChangeSpy).toHaveBeenCalled();
+		expect(setStateSpy).toHaveBeenCalledWith({ value: newTime });
+	});
+
 	it('calls a callback with value if one is provided', function() {
-		const time = '23:00';
-		TestUtils.Simulate.change(timeInputEl, { target: { value: time } });
-		expect(callbackSpy).toHaveBeenCalledWith(time);
+		TestUtils.Simulate.change(timeInputEl, { target: { value: newTime } });
+		expect(callbackSpy).toHaveBeenCalledWith(newTime);
 	});
 });
 
