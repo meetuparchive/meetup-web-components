94c48ab61086389b84e490e27384a6ecee64256f
ivy <unjust@gmail.com>
2017-02-28 16:15:02 -0500

    stubbing out test

--- src/dateTimePicker.test.jsx
+++ src/dateTimePicker.test.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import DateTimePicker from './DateTimePicker';
+// import DateTimeLocalInput from './DateTimeLocalInput';
+import FlatPickrComponent from './FlatPickrComponent';
+// import TimeInput from './TimeInput';
 
 describe('DateTimePicker', function() {
 
-	it('exists', function() {
-		const dateTimePicker = TestUtils.renderIntoDocument(<DateTimePicker name='start_time' />);
-		const dateTimePickerNode = ReactDOM.findDOMNode(dateTimePicker);
-
-		expect(dateTimePickerNode).not.toBeNull();
+	let dateTimeComponent;
+		// flatpickrComponent,
+		// timeComponent,
+		// dateTimeLocalComponent,
+		// dateEl,
+		// timeEl;
+	/*
+	beforeEach(() => {
+		dateTimeComponent = TestUtils.renderIntoDocument(<DateTimePicker name='start_time' forceFlatpickr />);
+		flatpickrComponent = TestUtils.findRenderedComponentWithType(dateTimeComponent, FlatPickrComponent);
+		dateTimeLocalComponent = TestUtils.findRenderedComponentWithType(dateTimeComponent, DateTimeLocalInput);
+		timeComponent = TestUtils.findRenderedComponentWithType(dateTimeComponent, TimeInput);
 	});
 
+	afterEach(() => {
+		dateTimeComponent = null;
+		timeComponent = null;
+		flatpickrComponent = null;
+		timeComponent = null;
+		dateEl = null;
+		timeEl = null;
+	});
+	*/
+
+	it('exists', function() {
+		dateTimeComponent = TestUtils.renderIntoDocument(<DateTimePicker name='start_time' forceFlatpickr />);
+		console.log(dateTimeComponent);
+		expect(() => TestUtils.findRenderedComponentWithType(dateTimeComponent, FlatPickrComponent)).not.toThrow();
+		// expect(() => TestUtils.findRenderedComponentWithType(dateTimeComponent, TimeInput)).not.toThrow();
+		// var child = ReactTestUtils.findRenderedComponentWithType(profile, Avatar);
+		// expect(ReactTestUtils.isCompositeComponentWithType(child, Avatar)).toBe(true);
+	});
+	/*
 	it('sets a default value for date and time', function() {});
 
 	it('allows for date only', function() {});
 
 	it('sets a date range to select from', function() {});
-
+	*/
 });
