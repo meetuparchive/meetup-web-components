38535128175e45ed2e03a9e82ebffb2ad2430124
mperrotti <mperrotti573@gmail.com>
2017-03-13 13:18:27 -0400

    handle no-photo state for AvatarMember, tried rendering SVG as base64

--- src/AvatarMember.jsx
+++ src/AvatarMember.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import cx from 'classnames';
 import Avatar from './Avatar';
+import { getIconAsBase64Uri } from './utils/getIconAsBase64Uri';
+
+const NO_PHOTO_SRC = getIconAsBase64Uri('profile');  // 'http://placehold.it/100x100'
 
 /**
  * An avatar for a member - just supply a member
@@ -21,15 +24,18 @@ class AvatarMember extends React.Component {
 			{
 				'avatar--org': org,
 				'avatar--fbFriend': fbFriend,
+				'avatar--noPhoto': (member.photo || {}).photo_link == undefined
 			},
 			className);
+
+		console.log(NO_PHOTO_SRC);
 
 		return (
 			<Avatar
 				alt={member.name}
-				src={(member.photo || {}).photo_link}
+				src={(member.photo || {}).photo_link || NO_PHOTO_SRC}
 				className={classNames}
-				{...other} />
+				{...other}/>
 		);
 	}
 }
