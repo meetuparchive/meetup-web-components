4fccd1370eaac1f0a75eb8016faea7771574a867
Adam Detrick <akdetrick@gmail.com>
2017-03-21 15:52:19 -0400

    use matching loader name in `require` for base64 loader

--- src/utils/base64.js
+++ src/utils/base64.js
@@ -4,5 +4,5 @@
  * @returns {string} base64-encoded data uri for SVG icon
  */
 export const getIconAsBase64Uri = name => {
-	return require(`base64-image!swarm-icons/dist/optimized/${name}.svg`);
+	return require(`base64-image-loader!swarm-icons/dist/optimized/${name}.svg`);
 };
