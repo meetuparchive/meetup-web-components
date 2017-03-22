69bcecb1b2fd782f721ad9e881f57779db710df4
Lori Hutchek <Lori.Hutchek@akqa.com>
2017-03-22 11:05:42 -0400

    adding icon test

--- src/avatarmember.test.js
+++ src/avatarmember.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import { MOCK_MEMBER } from 'meetup-web-mocks/lib/api';
 import AvatarMember, { AVATAR_PERSON_CLASS, AVATAR_PERSON_NOPHOTO_CLASS} from './AvatarMember';
+import Icon from './Icon';
 
 describe('AvatarMember', function() {
 	it('exists', function() {
@@ -20,11 +21,13 @@ describe('AvatarMember', function() {
 
 		const avatarMember = TestUtils.renderIntoDocument(<AvatarMember member={MOCK_MEMBER_NO_PHOTO} />);
 		expect(() => TestUtils.findRenderedDOMComponentWithClass(avatarMember, AVATAR_PERSON_NOPHOTO_CLASS)).not.toThrow();
+		expect(() => TestUtils.findRenderedComponentWithType(avatarMember, Icon)).not.toThrow();
 	});
 
 	it('should *not* render the noPhoto variant only when a photo is present', function() {
 		const avatarMember = TestUtils.renderIntoDocument(<AvatarMember member={MOCK_MEMBER} />);
 		expect(() => TestUtils.findRenderedDOMComponentWithClass(avatarMember, AVATAR_PERSON_NOPHOTO_CLASS)).toThrow();
+		expect(() => TestUtils.findRenderedComponentWithType(avatarMember, Icon)).toThrow();
 	});
 
 	it('applies variant classes for each variant prop', function() {
