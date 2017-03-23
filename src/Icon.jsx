aab66cff0f302aa4dba2a20773232bd4d4b36f58
mperrotti <mperrotti573@gmail.com>
2017-03-23 12:59:50 -0400

    WIP - first try and syncing media sizes and making Icon responsive

--- src/Icon.jsx
+++ src/Icon.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import cx from 'classnames';
+import { changeSizeWithViewport } from './utils/designConstants';
+import MediaListener from './utils/mediaQueryListener';
 
 export const ICON_CLASS = 'svg';
-
-export const MEDIA_SIZES = {
-	xs: '16',
-	s: '24',
-	m: '36',
-	l: '48',
-	xl: '72',
-};
 
 /**
  * Icon component used to insert an svg icon into a component or page
@@ -21,6 +15,38 @@ export const MEDIA_SIZES = {
  * @module Icon
  */
 class Icon extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this._onMediaQueryChange = this._onMediaQueryChange.bind(this);
+
+		this.state = {
+			currentBP: undefined
+		};
+	}
+
+	_onMediaQueryChange(name) {
+		this.setState({
+			currentBP: name
+		});
+	}
+
+	componentDidMount() {
+		this.mediaListener = new MediaListener(this._onMediaQueryChange);
+
+		// console.log(`getTheBP: ${this.mediaListener.getTheBP()}`);
+		// console.log(`getCurrentBP: ${this.mediaListener.getCurrentBP()}`);
+
+		this.setState({
+			currentBP: this.mediaListener.getCurrentBP()
+		});
+	}
+
+	componentWillUnmount() {
+		if (this.mediaListener) {
+			this.mediaListener.stopListening();
+		}
+	}
 
 	render() {
 		const {
@@ -36,7 +62,7 @@ class Icon extends React.Component {
 			className
 		);
 
-		const dim = MEDIA_SIZES[size];
+		const dim = changeSizeWithViewport(size, this.state.currentBP);
 
 		return (
 			<span className={classNames}>
@@ -56,7 +82,7 @@ class Icon extends React.Component {
 }
 
 Icon.defaultProps = {
-	size: 'm'
+	size: 's'
 };
 
 Icon.propTypes = {
