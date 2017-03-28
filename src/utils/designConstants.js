21a55e2ebbdf65f9ee4aaab8811ddd157c56948d
Adam Detrick <akdetrick@gmail.com>
2017-03-28 13:06:22 -0400

    add designConstants

---
+++ src/utils/designConstants.js
@@ -0,0 +1,15 @@
+export const BREAKPOINTS = {
+	s: '440px',
+	m: '640px',
+	l: '840px',
+	xl: '1024px'
+};
+
+export const MEDIA_SIZES = {
+	xs: 16,
+	s: 24,
+	m: 36,
+	l: 48,
+	xl: 72,
+	xxl: 120
+};
