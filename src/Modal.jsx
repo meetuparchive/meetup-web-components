5e738df4a2ba170fa792eb884d5cf8a3f959231a
Adam Detrick <akdetrick@gmail.com>
2017-03-28 20:18:22 -0400

    use matchMedia to reposition modal based on viewport size

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import cx from 'classnames';
 import Icon from './Icon';
 import Button from './Button';
+import { MEDIA_QUERIES } from './utils/designConstants';
 
 export const MODAL_CLOSE_BUTTON = 'modal-closeButton';
 const DEFAULT_MARGIN_TOP = '10vh';
+
 
 /**
  * SQ2 Modal component
@@ -16,10 +18,24 @@ class Modal extends React.Component {
 		super(props);
 		this.onDismiss = this.onDismiss.bind(this);
 		this.onKeyDown = this.onKeyDown.bind(this);
+		this.getModalPosition = this.getModalPosition.bind(this);
+
+		this.supportsMatchMedia = typeof window.matchMedia != 'undefined';
 
 		this.state = {
 			topPosition: DEFAULT_MARGIN_TOP // matches default margin-top in CSS
 		};
+
+		if (!this.props.fullscreen && this.supportsMatchMedia) {
+			this.mediaQuery = window.matchMedia(MEDIA_QUERIES.medium);
+
+			this.handleMediaChange = mq => {
+				this.setState({
+					topPosition: this.getModalPosition(window.pageYOffset),
+				});
+			};
+			this.mqListener = this.mediaQuery.addListener(this.handleMediaChange);
+		}
 	}
 
 	onDismiss(e) {
@@ -42,20 +58,26 @@ class Modal extends React.Component {
 	 */
 	getModalPosition(scrollPosition) {
 		const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		const isFullScreen = this.props.fullscreen || this.mediaQuery && !this.mediaQuery.matches;
 
-		if (scrollPosition < viewportHeight) {
-			return DEFAULT_MARGIN_TOP;
-		} else {
-			return this.props.fullscreen ?
-				'0px' :
-				scrollPosition + 20;
-		}
+		// check if user is scrolled below fold before setting a custom offset
+		const newTopOffset = scrollPosition > viewportHeight ?
+			DEFAULT_MARGIN_TOP :
+			scrollPosition + 20;
+
+		return isFullScreen ? '0px' : newTopOffset;
 	}
 
 	componentDidMount() {
 		this.setState({
 			topPosition: this.getModalPosition(window.pageYOffset)
 		});
+	}
+
+	componentWillUnmount() {
+		if (!this.props.fullscreen && this.supportsMatchMedia) {
+			this.mediaQuery.removeListener(this.handleMediaChange);
+		}
 	}
 
 	render() {
