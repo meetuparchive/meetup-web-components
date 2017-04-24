18cc607b552ac62046bda5d6026255ef80bec443
mperrotti <mperrotti573@gmail.com>
2017-04-24 12:39:30 -0400

    small changes, mostly to Section. pushing up for Adam to review

--- src/layout/Section.jsx
+++ src/layout/Section.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import cx from 'classnames';
 
 export const SECTION_CLASS = 'section';
-export const SECTION_PADDED_CLASS = 'section--padded';
+export const SECTION_NOBORDER_CLASS = 'section--noBorder';
+export const VALID_BREAKPOINTS = {
+	all: 'atAll',
+	medium: 'atMedium',
+	large: 'atLarge',
+};
+
 /**
  * Design System Component: Provides `section` container for components
  * @module Section
@@ -12,14 +18,16 @@ class Section extends React.Component {
 		const {
 			children,
 			className,
-			padded,
+			noBorder,
+			canBeLastChild,
 			...other
 		} = this.props;
 
 		const classNames = cx(
 			SECTION_CLASS,
 			{
-				[SECTION_PADDED_CLASS]: padded
+				[SECTION_NOBORDER_CLASS]: noBorder,
+				[`${VALID_BREAKPOINTS[canBeLastChild]}_${SECTION_CLASS}--canBeLastChild`]: canBeLastChild,
 			},
 			className
 		);
@@ -35,7 +43,8 @@ class Section extends React.Component {
 	}
 }
 Section.propTypes = {
-	padded:React.PropTypes.bool
+	noBorder: React.PropTypes.bool,
+	canBeLastChild: React.PropTypes.oneOf(Object.keys(VALID_BREAKPOINTS)),
 };
 
 export default Section;
--- src/layout/section.story.jsx
+++ src/layout/section.story.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { InfoWrapper } from '../utils/storyComponents';
 import Section from './Section';
+import Flex from './Flex';
+import FlexItem from './FlexItem';
 
 const shadingStyles = {
 	backgroundColor: 'rgba(220, 192, 255, 0.45)',
@@ -18,47 +20,104 @@ storiesOf('Section', module)
 				<div style={{height: '100vh', width: '100%', marginBottom: '20px'}}>
 					<div style={{maxWidth: '850px', margin: 'auto', marginTop:'15px'}}>
 						<Section style={shadingStyles}>
-							<h2 className='text--display2'>About us</h2>
-								<div className='runningText'>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-									<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-								</div>
-							<ul className='inlineblockList'><li><a className='token token--link' href='/topics/91146/tech-talks'>Tech Talks</a></li><li><a className='token token--link' href='/topics/10209/web'>Web Technology</a></li><li><a className='token token--link' href='/topics/16216/mobile-technology'>Mobile Technology</a></li><li><a className='token token--link' href='/topics/3833/softwaredev'>Software Development</a></li><li><a className='token token--link' href='/topics/48471/computer-programming'>Computer programming</a></li><li><a className='token token--link' href='/topics/127567/ios-development'>iOS Development</a></li><li><a className='token token--link' href='/topics/20346/android-developers'>Android Development</a></li><li><a className='token token--link' href='/topics/1040/ruby'>Ruby</a></li><li><a className='token token--link' href='/topics/563/opensource'>Open Source</a></li><li><a className='token token--link' href='/topics/189/java'>Java</a></li></ul>
+							<div className='chunk'>
+								<h2 className='text--display2'>About us</h2>
+									<div className='runningText'>
+										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+										<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+									</div>
+								<ul className='inlineblockList'><li><a className='token token--link' href='/topics/91146/tech-talks'>Tech Talks</a></li><li><a className='token token--link' href='/topics/10209/web'>Web Technology</a></li><li><a className='token token--link' href='/topics/16216/mobile-technology'>Mobile Technology</a></li><li><a className='token token--link' href='/topics/3833/softwaredev'>Software Development</a></li><li><a className='token token--link' href='/topics/48471/computer-programming'>Computer programming</a></li><li><a className='token token--link' href='/topics/127567/ios-development'>iOS Development</a></li><li><a className='token token--link' href='/topics/20346/android-developers'>Android Development</a></li><li><a className='token token--link' href='/topics/1040/ruby'>Ruby</a></li><li><a className='token token--link' href='/topics/563/opensource'>Open Source</a></li><li><a className='token token--link' href='/topics/189/java'>Java</a></li></ul>
+							</div>
 						</Section>
 
 						<Section style={shadingStyles}>
-							<h2 className='text--display2'>Organized by</h2>
-							<ul className='inlineblockList inlineblockList--bulleted'>
-								<li>Ana</li>
-								<li>Maria</li>
-								<li>Helen Ford</li>
-								<li>K Rattan</li>
-								<li>Jenn</li>
-								<li>Michael Colby</li>
-								<li>Sarah Hall</li>
-								<li>Viki</li>
-							</ul>
+							<div className='chunk'>
+								<h2 className='text--display2'>Organized by</h2>
+								<ul className='inlineblockList inlineblockList--bulleted'>
+									<li>Ana</li>
+									<li>Maria</li>
+									<li>Helen Ford</li>
+									<li>K Rattan</li>
+									<li>Jenn</li>
+									<li>Michael Colby</li>
+									<li>Sarah Hall</li>
+									<li>Viki</li>
+								</ul>
+							</div>
 						</Section>
 					</div>
 				</div>
 			</InfoWrapper>
 		)
 	)
-	.add('Padded', () => (
+	.add('No border', () => (
 		<div style={{width: '100%'}}>
 			<div style={{maxWidth: '850px', margin: 'auto'}}>
-				<Section padded style={shadingStyles}>
+				<Section noBorder style={shadingStyles}>
+					<div className='chunk'>
 						<h2 className='text--display2'>These sections are padded on all sides</h2>
+					</div>
 				</Section>
-				<Section padded style={shadingStyles}>
+				<Section noBorder style={shadingStyles}>
+					<div className='chunk'>
 						<p className='text--bold'>123 attending</p>
 						<p>Hosted by Amy, Rick, Mike, Natalie</p>
+					</div>
 				</Section>
-				<Section padded style={shadingStyles}>
+				<Section noBorder style={shadingStyles}>
+					<div className='chunk'>
 						<div className='runningText'>
 							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 						</div>
+					</div>
 				</Section>
 			</div>
+		</div>
+	))
+	.add('canBeLastChild', () => (
+		<div style={{width: '100%'}}>
+			<Flex direction='column' switchDirection='large'>
+				<FlexItem>
+					<div style={{maxWidth: '850px', margin: 'auto'}}>
+						<Section>
+							<div className='chunk'>
+								<h2 className='text--display666'>These sections are padded on all sides</h2>
+							</div>
+						</Section>
+						<Section>
+							<div className='chunk'>
+								<h2 className='text--display666'>These sections are padded on all sides</h2>
+							</div>
+						</Section>
+						<Section> {/* canBeLastChild='large' className='section--notAlwaysLast'*/}
+							<div className='chunk'>
+								<p className='text--bold'>123 attending</p>
+								<p>Hosted by Amy, Rick, Mike, Natalie</p>
+							</div>
+						</Section>
+					</div>
+				</FlexItem>
+
+				<FlexItem>
+					<div style={{maxWidth: '850px', margin: 'auto'}}>
+						<Section>
+							<div className='chunk'>
+								<h2 className='text--display666'>These sections are padded on all sides</h2>
+							</div>
+						</Section>
+						<Section>
+							<div className='chunk'>
+								<h2 className='text--display666'>These sections are padded on all sides</h2>
+							</div>
+						</Section>
+						<Section> {/* canBeLastChild='large'*/}
+							<div className='chunk'>
+								<p className='text--bold'>123 attending</p>
+								<p>Hosted by Amy, Rick, Mike, Natalie</p>
+							</div>
+						</Section>
+					</div>
+				</FlexItem>
+			</Flex>
 		</div>
 	));
