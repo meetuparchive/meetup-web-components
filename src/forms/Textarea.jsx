715edecdefabb25cc761fd52bca77acc02098175
Lori Hutchek <lori@meetup.com>
2017-04-26 21:51:32 -0400

    WC-45: adding value prop to textarea (#194)
    * adjusting textarea to allow for value to be passed
    
    * refining test
    
    * extracting to value var
    
    * updating based on suggestion

--- src/forms/Textarea.jsx
+++ src/forms/Textarea.jsx
@@ -8,26 +8,64 @@ import autosize from 'autosize';
 class Textarea extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			value: props.value || '',
-		};
 		this.onChange = this.onChange.bind(this);
+
+		this.state = {
+			value: '',
+		};
 	}
 
-	onChange(e) {
-		const { onChange } = this.props;
-		this.setState({
-			value: e.target.value,
-		});
-
-		if (onChange) {
-			onChange(e);
-		}
-	}
-
+	/**
+	 * Turns on autosize if requested
+	 * @return {undefined} side effect only
+	 */
 	componentDidMount() {
 		if (this.props.rows === 'auto'){
 			autosize(this.textarea);
+		}
+	}
+
+	/**
+	 * Should override value with info from state
+	 * @return {[type]} [description]
+	 */
+	overrideValue (nextProps) {
+		this.setState(() => ({
+			value: nextProps.value || ''
+		}));
+	}
+
+	/**
+	 * @param {Object} props the incoming props
+	 * @return {undefined} side effect only
+	 */
+	componentWillMount() {
+		this.overrideValue(this.props);
+	}
+
+	/**
+	 * @param {Object} nextProps the incoming props
+	 * @return {undefined} side effect only
+	 */
+	componentWillReceiveProps(nextProps) {
+		this.overrideValue(nextProps);
+	}
+
+	/**
+	 * called as user changes value, updates state with new value
+	 * @param  {Object} e Event object
+	 * @return {undefined}
+	 */
+	onChange(e) {
+		const { onChange } = this.props;
+		const value = e.target.value;
+
+		this.setState(() => ({
+			value,
+		}));
+
+		if (onChange) {
+			onChange(e);
 		}
 	}
 
@@ -73,7 +111,8 @@ class Textarea extends React.Component {
 				<label className={classNames.label} htmlFor={id}>
 					{label}
 				</label>
-				<textarea type='text'
+				<textarea
+					type='text'
 					name={name}
 					required={required}
 					className={classNames.textarea}
@@ -81,8 +120,8 @@ class Textarea extends React.Component {
 					rows={rows == 'auto' ? 1 : rows}
 					ref={(textarea) => {this.textarea = textarea;}}
 					style={{ ...style, ...heightConstraints }}
-					value={this.state.value}
 					id={id}
+					value={this.state.value}
 					{...other}
 				/>
 
@@ -113,7 +152,8 @@ Textarea.propTypes = {
 	rows: React.PropTypes.oneOfType([
 		React.PropTypes.number,
 		React.PropTypes.string
-	])
+	]),
+	value: React.PropTypes.string,
 };
 
 export default Textarea;
