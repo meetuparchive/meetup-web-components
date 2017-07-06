de25dafa6e38e28151297a0efea18ea58e8274c5
ivy <ivy@meetup.com>
2017-07-06 14:17:01 -0400

    adding placeholder rendering and intl injection

--- src/forms/textInput.story.jsx
+++ src/forms/textInput.story.jsx
@@ -2,37 +2,55 @@ import React from 'react';
 import TextInput from './TextInput';
 import Button from './Button';
 import { storiesOf } from '@kadira/storybook';
-import {
-	IntlProvider,
-	FormattedMessage,
-	defineMessages,
-} from 'react-intl';
+import { IntlProvider, FormattedMessage, defineMessages } from 'react-intl';
 
 storiesOf('TextInput', module)
-	.add('default', () => (<TextInput
-		label='Your name'
-		id='fullname'
-		name='name'
-		placeholder='Not your email' />))
-	.add('with value', () => (<TextInput
-		label='Your name'
-		id='fullname'
-		name='name'
-		value='Phife Dawg'
-		placeholder='Not your email' />))
-	.add('disabled', () => (<TextInput
-		label='Your name'
-		id='fullname'
-		name='name'
-		value='Phife Dawg'
-		placeholder='Not your email'
-		disabled />))
-	.add('error state', () => (<TextInput
-		label='Your name'
-		id='fullname'
-		name='name'
-		error='Not so fast. You have an error.'
-		placeholder='Not your email' />))
+	.add('default', () => (
+		<IntlProvider defaultLocale='en-US' locale='en-US'>
+			<div className='span--50'>
+				<TextInput
+					label='Your name'
+					id='fullname'
+					name='name'
+					placeholder='Not your email' />
+			</div>
+		</IntlProvider>)
+	)
+	.add('with value', () => (
+		<IntlProvider defaultLocale='en-US' locale='en-US'>
+			<div className='span--50'>
+				<TextInput
+					label='Your name'
+					id='fullname'
+					name='name'
+					value='Phife Dawg'
+					placeholder='Not your email' />
+			</div>
+		</IntlProvider>)
+	)
+	.add('disabled', () => (
+		<IntlProvider defaultLocale='en-US' locale='en-US'>
+			<div className='span--50'>
+				<TextInput
+					label='Your name'
+					id='fullname'
+					name='name'
+					value='Cannot focus'
+					disabled />
+			</div>
+		</IntlProvider>)
+	)
+	.add('error state', () => (
+		<IntlProvider defaultLocale='en-US' locale='en-US'>
+			<div className='span--50'>
+				<TextInput
+					label='Your name'
+					id='fullname'
+					name='name'
+					error='Not so fast. You have an error.' />
+			</div>
+		</IntlProvider>)
+	)
 	.add('error state formatted', () => {
 		const trn = defineMessages({
 			error: {
@@ -43,74 +61,91 @@ storiesOf('TextInput', module)
 		});
 		return (
 			<IntlProvider defaultLocale='en-US' locale='en-US'>
-				<TextInput label='Your name'
-					id='fullname'
-					name='name'
-					error={<FormattedMessage {...trn.error} />}
-					placeholder='Not your email' />
+				<div className='span--50'>
+					<TextInput label='Your name'
+						id='fullname'
+						name='name'
+						error={<FormattedMessage {...trn.error} />} />
+				</div>
 			</IntlProvider>
 		);
 	})
 	.add('required', () => {
-		return (<form>
-			<TextInput
-				label='Your name'
-				id='fullname'
-				name='name'
-				required
-				placeholder='Not your email' />
-			<Button
-				contrast
-				fullWidth>
-				Submit
-			</Button>
-		</form>);
+		return (
+			<IntlProvider defaultLocale='en-US' locale='en-US'>
+				<form className='span--50'>
+					<TextInput
+						label='Your name'
+						id='fullname'
+						name='name'
+						required
+						placeholder='Not your email' />
+					<Button
+						contrast
+						fullWidth>
+						Submit
+					</Button>
+				</form>
+			</IntlProvider>
+		);
 	})
 	.add('search', () => {
-		return (<form>
-			<TextInput
-				label='Search the world wide web'
-				id='search'
-				name='search'
-				placeholder='What will it be?'
-				isSearch />
-			<Button
-				contrast
-				fullWidth>
-				Submit
-			</Button>
-		</form>);
+		return (
+			<IntlProvider defaultLocale='en-US' locale='en-US'>
+				<form>
+					<TextInput
+						label='Search the world wide web'
+						id='search'
+						name='search'
+						placeholder='What will it be?'
+						isSearch />
+					<Button
+						contrast
+						fullWidth>
+						Submit
+					</Button>
+				</form>
+			</IntlProvider>
+		);
 	})
 	.add('with char counter (maxLength)', () => {
 		const rules = {
 			maxLength: 10,
 			pattern:'.{5,10}'
 		};
-		return (<TextInput
-			label='Your name'
-			id='fullname'
-			name='name'
-			value=''
-			placeholder='Not your email'
-			{...rules} />);
+		return (
+			<IntlProvider defaultLocale='en-US' locale='en-US'>
+				<div className='span--50'>
+					<TextInput
+						label='Your name'
+						id='fullname'
+						name='name'
+						value=''
+						{...rules} />
+				</div>
+			</IntlProvider>
+		);
 	})
 	.add('has a pattern for min length', () => {
 		const rules = {
 			pattern:'.{5,10}'
 		};
-		return (<form>
-			<TextInput
-				label='Your name'
-				id='fullname'
-				name='name'
-				value='>5'
-				placeholder='Not your email'
-				{...rules} />
-			<Button
-				contrast
-				fullWidth>
-				Submit
-			</Button>
-		</form>);
+		return (
+			<IntlProvider defaultLocale='en-US' locale='en-US'>
+				<form>
+					<TextInput
+						label='Your name'
+						id='fullname'
+						name='name'
+						value='>5'
+						{...rules} />
+					<Button
+						contrast
+						fullWidth>
+						Submit
+					</Button>
+				</form>
+			</IntlProvider>
+		);
 	});
 
