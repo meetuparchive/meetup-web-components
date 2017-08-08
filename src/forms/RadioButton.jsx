9c63e4fcd68aeda4219f30e75b2874a25ec38130
mperrotti <mperrotti573@gmail.com>
2017-08-08 17:30:53 -0400

    stylez 💅

--- src/forms/RadioButton.jsx
+++ src/forms/RadioButton.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import cx from 'classnames';
 import Flex from '../layout/Flex';
 import FlexItem from '../layout/FlexItem';
 
 const RadioButton = ({ label, className, ...inputProps }) => {
 	const id = `RadioButton-${inputProps.name}-${inputProps.value}`;
 
+	const classNames = {
+		labelClassNames: cx(
+			'toggleLabel label--minor display--block',
+			inputProps.labelClassName
+		),
+		fauxCheckboxClassNames: cx(
+			'display--flex flex--alignCenter flex--center align--center fauxToggle fauxToggle--radio',
+			{
+				['checked'] : inputProps.checked,
+				['disabled'] : inputProps.disabled
+			}
+		)
+	};
+
 	return (
-		<Flex align="center" className={className} noGutters>
-			<FlexItem shrink>
-				<input readOnly type="radio" id={id} {...inputProps} />
-			</FlexItem>
-			<FlexItem shrink>
-				<label htmlFor={id}>
-					{label}
-				</label>
-			</FlexItem>
-		</Flex>
+		<label className={classNames.labelClassNames} htmlFor={id}>
+			<Flex align="center" className={className} noGutters>
+				<FlexItem shrink>
+					<input readOnly type="radio" className="radio visibility--a11yHide" id={id} {...inputProps} />
+					<span className={classNames.fauxCheckboxClassNames}>
+						{inputProps.checked &&
+							<span className="radio-indicator"></span>
+						}
+					</span>
+				</FlexItem>
+				<FlexItem className='toggleLabel-container'>
+					<span className={cx({
+						['text--hint'] : inputProps.disabled,
+						['text--bold'] : inputProps.checked
+					})}>
+						{label}
+					</span>
+				</FlexItem>
+			</Flex>
+		</label>
 	);
 };
 
