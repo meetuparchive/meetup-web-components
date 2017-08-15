c32a42a99e58490691c788ed51c3436021787662
ivy <ivy@meetup.com>
2017-08-15 16:41:24 -0400

    update test and snap to test ReduxTextInput

--- src/forms/redux-form/reduxTextInput.test.jsx
+++ src/forms/redux-form/reduxTextInput.test.jsx
@@ -1,37 +1,24 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
 import { shallow } from 'enzyme';
 import ReduxTextInput from './ReduxTextInput';
 
 describe('ReduxTextInput', function() {
 
-	const formName = 'testForm',
-		formAttrs = {
+	const formAttrs = {
+		input: {
 			label: 'Super Hero',
 			name: 'superhero',
 			value: 'Wonder Woman and Robin',
 			maxLength: 20,
-			required: true,
+			required: true
+		},
+		meta: {
 			error: 'Did you mean Batman and Robin?'
-		};
-
-	// eslint-disable-next-line require-jsdoc
-	class FormComponent extends React.Component {
-		render() {
-			return <form name={formName}>{this.props.children}</form>;
 		}
-	}
+	};
 
-	const DecoratedFormComponent = reduxForm({
-		form: formName,
-	})(FormComponent);
-
-	it('renders a required HTML <input> with expected attributes for mock data', () => {
-		const component = shallow(
-			<DecoratedFormComponent>
-				<Field component={ReduxTextInput} {...formAttrs} />
-			</DecoratedFormComponent>
-		);
+	it('renders a TextInput component with expected attributes from mock data', () => {
+		const component = shallow(<ReduxTextInput {...formAttrs} />);
 
 		expect(component).toMatchSnapshot();
 
