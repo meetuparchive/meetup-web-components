27821643f4e8bd7391b3e6985531198564fa6df9
mperrotti <mperrotti573@gmail.com>
2017-09-05 12:35:24 -0400

    added flush modifiers and story

--- src/layout/Card.jsx
+++ src/layout/Card.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 import cx from 'classnames';
 
 export const CARD_CLASS = 'card';
+export const CARD_FLUSH_CLASS = `${CARD_CLASS}--flush`;
+export const CARD_HOVER_PLUS_SHADOW_CLASS = `${CARD_CLASS}--hasShadowPlusHover`;
+export const VALID_BREAKPOINTS = {
+	all: 'atAll',
+	medium: 'atMedium',
+	large: 'atLarge',
+};
 
 /**
  * @module Card
@@ -15,16 +22,20 @@ class Card extends React.PureComponent {
 			initialHeight,
 			hasShadow,
 			hasHoverShadow,
+			flushUntil,
 			...other
 		} = this.props;
+
+		const flushBreakpoint = VALID_BREAKPOINTS[flushUntil] || VALID_BREAKPOINTS['all'];
 
 		const classNames = cx(
 			CARD_CLASS,
 			{
 				[`${CARD_CLASS}--initialHeight`]: initialHeight,
+				[`${flushBreakpoint}_${CARD_FLUSH_CLASS} ${CARD_FLUSH_CLASS}`]: flushUntil,
 				[`${CARD_CLASS}--hasShadow`]: hasShadow,
 				[`${CARD_CLASS}--hasHoverShadow`]: hasHoverShadow && !hasShadow,
-				[`${CARD_CLASS}--hasShadowPlusHover`]: hasHoverShadow && hasShadow
+				[CARD_HOVER_PLUS_SHADOW_CLASS]: hasHoverShadow && hasShadow
 			},
 			className
 		);
@@ -43,6 +54,7 @@ Card.propTypes = {
 	initialHeight: PropTypes.bool,
 	hasShadow: PropTypes.bool,
 	hasHoverShadow: PropTypes.bool,
+	flushUntil: PropTypes.oneOfType([PropTypes.oneOf(Object.keys(VALID_BREAKPOINTS))])
 };
 
 export default Card;
