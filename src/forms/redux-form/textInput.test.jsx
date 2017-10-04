f8938bba1482801182509d704b9884c8c774f83d
Yanyi Luo <yanyi@meetup.com>
2017-10-04 12:15:28 -0400

    add validateAfterTouched option

--- src/forms/redux-form/textInput.test.jsx
+++ src/forms/redux-form/textInput.test.jsx
@@ -4,6 +4,7 @@ import ReduxFormTextInput from './TextInput';
 
 describe('redux-form TextInput', function() {
 
+	const MOCK_ERROR = 'Did you mean Batman and Robin?';
 	const formAttrs = {
 		input: {
 			label: 'Super Hero',
@@ -13,7 +14,7 @@ describe('redux-form TextInput', function() {
 			required: true
 		},
 		meta: {
-			error: 'Did you mean Batman and Robin?'
+			error: MOCK_ERROR
 		}
 	};
 
@@ -21,7 +22,39 @@ describe('redux-form TextInput', function() {
 		const component = shallow(<ReduxFormTextInput {...formAttrs} />);
 
 		expect(component).toMatchSnapshot();
+	});
 
+	describe('validateAfterTouched', () => {
+		it('always renders the error when `validateAfterTouched` is not true', () => {
+			const component = shallow(<ReduxFormTextInput {...formAttrs} />);
+			expect(component.prop('error')).toBe(MOCK_ERROR);
+		});
+		describe('when true', () => {
+			it('does not render the error when touched is false', () => {
+				const props = {
+					...formAttrs,
+					meta: {
+						touched: false,
+						error: MOCK_ERROR
+					},
+					validateAfterTouched: true
+				};
+				const component = shallow(<ReduxFormTextInput {...props} />);
+				expect(component.prop('error')).toBe(null);
+			});
+			it('does render the error when touched is true', () => {
+				const props = {
+					...formAttrs,
+					meta: {
+						touched: true,
+						error: MOCK_ERROR
+					},
+					validateAfterTouched: true
+				};
+				const component = shallow(<ReduxFormTextInput {...props} />);
+				expect(component.prop('error')).toBe(MOCK_ERROR);
+			});
+		});
 	});
 
 });
