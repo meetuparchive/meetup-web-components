4a9d44578da56ae3565206b57a86266ca0915e0a
Michael McGahan <mike.mcgahan@gmail.com>
2017-11-02 13:23:30 +1300

    redux-form-compatible NumberInput

---
+++ src/forms/redux-form/numberInput.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ReduxFormNumberInput from './NumberInput';
+
+describe('redux-form NumberInput', function() {
+	// props structured to match what redux-form provides to <Field> `component`
+	const props = {
+		input: {
+			label: 'Number of internets',
+			name: 'internets',
+			value: 10,
+		},
+	};
+
+	it('renders a NumberInput component with expected attributes from mock data', () => {
+		const component = shallow(<ReduxFormNumberInput {...props} />);
+		expect(component).toMatchSnapshot();
+	});
+});
