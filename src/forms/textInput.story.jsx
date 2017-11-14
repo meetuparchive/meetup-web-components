2de2400624ec07f9725f26bbd44b344eff2ddf82
Mike Perrotti <mikep@meetup.com>
2017-11-14 17:21:22 -0500

    upgrade sasstools

--- src/forms/textInput.story.jsx
+++ src/forms/textInput.story.jsx
@@ -59,6 +59,17 @@ storiesOf('TextInput', module)
 			</div>
 		);
 	})
+	.add('with helper text', () => (
+		<div className='span--50'>
+			<TextInput
+				label='Your name'
+				id='fullname'
+				name='name'
+				placeholder='Not your email'
+				helperText='Names cannot contain special characters' />
+		</div>
+	)
+	)
 	.add('required', () => {
 		return (
 			<form className='span--50'>
@@ -128,17 +139,6 @@ storiesOf('TextInput', module)
 				</InfoWrapper>
 			);
 		}
-	)
-	.add('with helper text', () => (
-		<div className='span--50'>
-			<TextInput
-				label='Your name'
-				id='fullname'
-				name='name'
-				placeholder='Not your email'
-				helperText='Names cannot contain special characters' />
-		</div>
-	)
 	)
 	.add('has a pattern for min length', () => {
 		const rules = {
