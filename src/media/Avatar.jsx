89f9e5d05742bfc6fc2d39292b431ead9ac4d34b
Adam Detrick <akdetrick@gmail.com>
2017-11-21 17:09:14 -0500

    add print-only avatar img tag

--- src/media/Avatar.jsx
+++ src/media/Avatar.jsx
@@ -29,7 +29,7 @@ class Avatar extends React.PureComponent {
 		} = this.props;
 
 		const classNames = cx(
-			'avatar',
+			'avatar HOTDOGS',
 			{
 				'avatar--small': small,
 				'avatar--large': large,
@@ -59,11 +59,20 @@ class Avatar extends React.PureComponent {
 			...other
 		};
 
+		// hidden by default;
+		// displayed only for print media
+		const printPhoto = src
+			&& <img
+					className="avatar-print"
+					src={src}
+					alt={alt}
+				/>;
+
 		if (this.props.to || this.props.href) {
-			return <a {...allProps}>{alt}{children}</a>;
+			return <a {...allProps}>{alt}{children}{printPhoto}</a>;
 		}
 
-		return <span {...allProps}>{alt}{children}</span>;
+		return <span {...allProps}>{alt}{children}{printPhoto}</span>;
 	}
 }
 
