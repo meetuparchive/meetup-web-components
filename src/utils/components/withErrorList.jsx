f23ad0d79068682a44b72d55920d6e44033a8668
Adam Detrick <akdetrick@gmail.com>
2017-12-11 13:54:56 -0500

    clean up withErrorList helper functions

--- src/utils/components/withErrorList.jsx
+++ src/utils/components/withErrorList.jsx
@@ -9,47 +9,8 @@ import ErrorList from '../../forms/ErrorList';
  * @param {string} id - the id of the form field
  * @returns {string} id for error list
  */
-const getErrorId = (id) => id && `${id}-error`;
+export const getErrorId = (id) => `${id}-error`;
 
-/**
- * @function getFieldErrorProps
- * returns an object of ARIA attributes to assign
- * to a form field with an error
- *
- * @param {string} id - the id of the form field
- * @param {boolean} hasError - pass `true` if field has an error
- * @returns {object} props object to apply to field
- */
-export const getFieldErrorProps = (id, hasError) => {
-	const ariaProps = {
-		'aria-invalid': !!hasError,
-	};
-
-	if (id) {
-		ariaProps['aria-describedby'] = getErrorId(id);
-	}
-
-	return ariaProps;
-};
-
-/**
- * @function getErrorListProps
- * returns props to pass for an ErrorList related
- * to a form field
- *
- * @param {string} id - the id of the form field
- * @param {string|array} errors - single or multiple error messages
- * @returns {object} props object to apply to ErrorList
- */
-export const getErrorListProps = (id, errors) => {
-	const props = { errors };
-
-	if (id) {
-		props['errorId'] = getErrorId(id);
-	}
-
-	return props;
-};
 
 /**
  * Provides `ErrorList` component and aria related props
@@ -82,10 +43,20 @@ const withErrorList = WrappedComponent => {
 				error,
 			} = this.props;
 
+			const fieldProps = {
+				id,
+				'aria-invalid': Boolean(error),
+				'aria-describedby': id && getErrorId(id),
+			};
+			const listProps = {
+				errorId: id && getErrorId(id),
+				errors: error,
+			};
+
 			return this.props.suppressError ? <WrappedComponent /> : (
 				<div>
-					<WrappedComponent {...this.props} {...getFieldErrorProps(id, !!error)} />
-					<ErrorList {...getErrorListProps(id, error)} />
+					<WrappedComponent {...this.props} {...fieldProps} />
+					<ErrorList {...listProps} />
 				</div>
 			);
 		}
