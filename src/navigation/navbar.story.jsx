15dd99e401d161661e6021770474f8d627338c81
Khristian Brooks <kh.brooks02@gmail.com>
2018-02-14 14:14:49 -0500

    Added SVG loader; Added Navbar, Notifications Dropdown, and Profile Dropdown

--- src/navigation/navbar.story.jsx
+++ src/navigation/navbar.story.jsx
@@ -7,10 +7,26 @@ import {
 
 import NavBar from './NavBar';
 
+const group = [{
+	id: 6622782,
+	name: "Whiskey Wednesdays",
+	status: "active",
+	link: "https://www.dev.meetup.com/Whiskey-Wednesdays/",
+	urlname: "Whiskey-Wednesdays",
+	description: "This is a description",
+}];
+
 storiesOf('NavBar', module)
 	.addDecorator(decorateWithLocale)
 	.addDecorator(decorateWithInfo)
 	.add('Default', () => (
-		<NavBar />
+		<NavBar
+			self={{
+				id: 1234,
+				name: 'John Q. Testington',
+				status: 'active'
+			}}
+			groups={group}
+		/>
 	)
 	);
