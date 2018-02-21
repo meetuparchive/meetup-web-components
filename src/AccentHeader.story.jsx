ab8811457662139e94845c5d30d3f5087c9dfb4a
Mike Perrotti <mikep@meetup.com>
2018-02-20 20:10:28 -0500

    wrote tests

--- src/AccentHeader.story.jsx
+++ src/AccentHeader.story.jsx
@@ -10,12 +10,21 @@ storiesOf('AccentHeader', module)
 			<p>This text is evenly spaced from the border</p>
 		</div>
 	))
-	.add('right-aligned', () =>
-		<AccentHeader align='right'>I have something to say</AccentHeader>
-	)
-	.add('center-aligned', () =>
-		<AccentHeader align='center'>I have something to say</AccentHeader>
-	)
+	.add('right-aligned', () => (
+		<div className="align--right">
+			<AccentHeader align='right'>I have something to say</AccentHeader>
+			<p>This text is evenly spaced from the border</p>
+		</div>
+	))
+	.add('center-aligned', () =>(
+		<div className="align--center">
+			<AccentHeader align='center'>I have something to say</AccentHeader>
+			<p>This text is evenly spaced from the border</p>
+		</div>
+	))
 	.add('custom heading tag', () =>
-		<AccentHeader headingTag='h1'>I'm wrapped in h1</AccentHeader>
+		(<div>
+			<AccentHeader headingTag='h1'>I'm wrapped in h1</AccentHeader>
+			<p>This text is evenly spaced from the border</p>
+		</div>)
 	);
