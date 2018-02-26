219d774f857c43123bd8cb5a8246745f4cf1dc8f
Khristian Brooks <kh.brooks02@gmail.com>
2018-02-26 18:18:35 -0500

    Added logos in nav definition

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import swarmLogo from '../../assets/svg/logo--mSwarm--2color.svg';
+import scriptLogo from '../../assets/svg/logo--script.svg';
 import withMatchMedia from '../utils/components/withMatchMedia';
 import Flex from '../layout/Flex';
+
+import NavItem from './NavItem';
 
 /**
  * @param {Object} props component properties
@@ -13,9 +17,14 @@ export class Nav extends React.Component {
 	 * @return {React.element} the navbar component
 	 */
 	render() {
-		const { media, self, unauthItems, authItems, logo } = this.props;
+		const { media, self, unauthItems, authItems, logoAccessible } = this.props;
 
 		const isLoggedOut = self.status === 'prereg' || !self.name;
+
+		const showScriptLogo = Boolean(media.isAtLargeUp || isLoggedOut);
+		const showSwarmLogo = Boolean(
+			media.isAtMediumUp && !media.isAtLargeUp && !isLoggedOut
+		);
 
 		return (
 			<nav
@@ -28,7 +37,31 @@ export class Nav extends React.Component {
 					justify="spaceBetween"
 					className="span--100 navBar"
 				>
-					{logo}
+					{showSwarmLogo && (
+						<NavItem
+							linkTo="meetup.com"
+							className="logo logo--swarm flush--left"
+							icon={
+								<img src={swarmLogo} alt={logoAccessible} height="48px" />
+							}
+						/>
+					)}
+
+					{showScriptLogo && (
+						<NavItem
+							linkTo="meetup.com"
+							className="logo logo--script flush--left"
+							linkClassName="display--block"
+							icon={
+								<img
+									src={scriptLogo}
+									alt={logoAccessible}
+									height="44px"
+								/>
+							}
+						/>
+					)}
+
 					{isLoggedOut ? unauthItems : authItems}
 				</Flex>
 			</nav>
@@ -36,12 +69,15 @@ export class Nav extends React.Component {
 	}
 }
 
+Nav.defaultProps = {
+	logoAccessible: 'Meetup Logo',
+};
 Nav.propTypes = {
 	self: PropTypes.object,
 	media: PropTypes.object,
 	authItems: PropTypes.array,
 	unauthItems: PropTypes.array,
-	logo: PropTypes.array,
+	logoAccessible: PropTypes.string,
 };
 
 export default withMatchMedia(Nav);
