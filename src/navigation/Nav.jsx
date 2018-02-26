10fc5722af6e81e0989f8324091177ce73986988
Khristian Brooks <kh.brooks02@gmail.com>
2018-02-26 17:10:22 -0500

    updated nav story, refactored logo as prop

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -1,12 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import swarmLogo from '../../assets/svg/logo--mSwarm--2color.svg';
-import scriptLogo from '../../assets/svg/logo--script.svg';
 import withMatchMedia from '../utils/components/withMatchMedia';
 import Flex from '../layout/Flex';
-
-import NavItem from './NavItem';
 
 /**
  * @param {Object} props component properties
@@ -17,14 +13,9 @@ export class Nav extends React.Component {
 	 * @return {React.element} the navbar component
 	 */
 	render() {
-		const { media, self, unauthItems, authItems } = this.props;
+		const { media, self, unauthItems, authItems, logo } = this.props;
 
 		const isLoggedOut = self.status === 'prereg' || !self.name;
-
-		const showScriptLogo = Boolean(media.isAtLargeUp || isLoggedOut);
-		const showSwarmLogo = Boolean(
-			media.isAtMediumUp && !media.isAtLargeUp && !isLoggedOut
-		);
 
 		return (
 			<nav
@@ -37,25 +28,7 @@ export class Nav extends React.Component {
 					justify="spaceBetween"
 					className="span--100 navBar"
 				>
-					{showSwarmLogo && (
-						<NavItem
-							linkTo="meetup.com"
-							className="logo logo--swarm flush--left"
-							icon={<img src={swarmLogo} alt="Meetup Logo" height="48px" />}
-						/>
-					)}
-
-					{showScriptLogo && (
-						<NavItem
-							linkTo="meetup.com"
-							className="logo logo--script flush--left"
-							linkClassName="display--block"
-							icon={
-								<img src={scriptLogo} alt="Meetup Logo" height="44px" />
-							}
-						/>
-					)}
-
+					{logo}
 					{isLoggedOut ? unauthItems : authItems}
 				</Flex>
 			</nav>
@@ -68,6 +41,7 @@ Nav.propTypes = {
 	media: PropTypes.object,
 	authItems: PropTypes.array,
 	unauthItems: PropTypes.array,
+	logo: PropTypes.array,
 };
 
 export default withMatchMedia(Nav);
--- src/navigation/nav.story.jsx
+++ src/navigation/nav.story.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { decorateWithLocale, decorateWithInfo } from '../utils/decorators';
 
+import { decorateWithLocale, decorateWithInfo } from '../utils/decorators';
+import scriptLogo from '../../assets/svg/logo--script.svg';
+import Icon from '../media/Icon';
 import NavItem from './NavItem';
 import Nav from './Nav';
 
 const CLASS_UNAUTH_ITEM = 'navItem--unauthenticated';
-// const CLASS_AUTH_ITEM = 'navItem--authenticated';
+const CLASS_AUTH_ITEM = 'navItem--authenticated';
 
 const unauthItems = [
 	<NavItem
@@ -24,17 +26,72 @@ const unauthItems = [
 		className={CLASS_UNAUTH_ITEM}
 	/>,
 ];
+const authItems = [
+	<NavItem
+		shrink
+		linkTo="meetup.com"
+		label="Explore"
+		className={`${CLASS_AUTH_ITEM} flush--left`}
+		icon={<Icon shape="search" size="s" className="atMedium_display--none" />}
+	/>,
+	<NavItem
+		shrink
+		label="Notifications"
+		className={`navItem--notifications ${CLASS_AUTH_ITEM}`}
+		dropdownContent={
+			<ul>
+				<li> Notif 1 </li>
+				<li> Notif 2 </li>
+				<li> Notif 3 </li>
+			</ul>
+		}
+	/>,
+	<NavItem
+		shrink
+		linkTo="meetup.com"
+		label="Messages"
+		className={`navItem--messages ${CLASS_AUTH_ITEM}`}
+		icon={
+			<Icon
+				shape="messages"
+				size="s"
+				className="display--block atMedium_display--none"
+			/>
+		}
+		hasUpdates
+	/>,
+];
+const logo = [
+	<NavItem
+		linkTo="meetup.com"
+		className="logo logo--script flush--left"
+		linkClassName="display--block"
+		icon={<img src={scriptLogo} alt="Meetup Logo" height="44px" />}
+	/>,
+];
 
 storiesOf('Nav', module)
 	.addDecorator(decorateWithLocale)
 	.addDecorator(decorateWithInfo)
-	.add('default', () => (
+	.add('authenticated', () => (
 		<Nav
 			self={{
 				id: 1234,
 				name: 'John Q. Testington',
 				status: 'active',
 			}}
-			authItems={unauthItems}
+			unauthItems={unauthItems}
+			authItems={authItems}
+			logo={logo}
+		/>
+	))
+	.add('unauthenticated', () => (
+		<Nav
+			self={{
+				status: 'prereg',
+			}}
+			unauthItems={unauthItems}
+			authItems={authItems}
+			logo={logo}
 		/>
 	));
