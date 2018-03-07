21d8e77f621a771617b53b7c2632735de918c894
Khristian Brooks <kh.brooks02@gmail.com>
2018-03-07 18:37:33 -0500

    Added notificationsDropdown to components

---
+++ src/navigation/components/notifications/NotificationsDropdown.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import {
+	MOCK_NOTIFICATION_COMMENT,
+	MOCK_NOTIFICATIONS_LIST,
+} from 'meetup-web-mocks/lib/notifications/api';
+
+import { NotificationsDropdownComponent } from './NotificationsDropdown';
+
+// add a member comment notifs without a photo
+// to the mock notifications list
+const notifs = [
+	...MOCK_NOTIFICATIONS_LIST,
+	{
+		...MOCK_NOTIFICATION_COMMENT,
+		photo: null,
+	},
+];
+
+describe('Notifications Dropdown', () => {
+	const wrapper = (props = {}) =>
+		shallow(
+			<NotificationsDropdownComponent
+				self={{ id: '1234' }}
+				notifications={notifs}
+				{...props}
+			/>
+		);
+
+	it('should match the snapshot with notifications ', () => {
+		expect(wrapper()).toMatchSnapshot();
+	});
+	it('should match the snapshot with an empty notifications', () => {
+		expect(wrapper({ notifications: [] })).toMatchSnapshot();
+	});
+});
--- src/navigation/components/profile/ProfileDropdown.jsx
+++ src/navigation/components/profile/ProfileDropdown.jsx
@@ -1,4 +1,3 @@
-// @flow
 import React from 'react';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
