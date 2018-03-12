166f25aaedfb2c6cbc20c07b8649e31c34886373
Khristian Brooks <kh.brooks02@gmail.com>
2018-03-12 12:51:56 -0400

    Updated test to be more extensive

--- src/navigation/Nav.test.jsx
+++ src/navigation/Nav.test.jsx
@@ -2,23 +2,57 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Nav from './Nav';
 
-const renderComponent = props => shallow(<Nav {...props} />);
+const wrapper = props => shallow(<Nav {...props} />);
 
 describe('Nav', () => {
-	const navSwarm = renderComponent({
-		media: { isAtMediumUp: true },
-		self: { status: 'active', name: 'Jeff Cleft' },
-	});
-	const navScript = renderComponent({
-		media: { isAtLargeUp: true },
-		self: { status: 'prereg' },
-	});
-
-	it('should match the snapshot for SwarmLogo', () => {
-		expect(navSwarm).toMatchSnapshot();
+	it('should match the snapshot for unauthenticated small screens', () => {
+		expect(
+			wrapper({
+				media: { isAtMediumUp: false },
+				self: { status: 'prereg' },
+			})
+		).toMatchSnapshot();
 	});
 
-	it('should match the snapshot for ScriptLogo', () => {
-		expect(navScript).toMatchSnapshot();
+	it('should match the snapshot for unauthenticated medium screens', () => {
+		expect(
+			wrapper({
+				media: { isAtMediumUp: false },
+				self: { status: 'active', name: 'Jeff Cleft' },
+			})
+		).toMatchSnapshot();
+	});
+
+	it('should match the snapshot for authenticated medium screens', () => {
+		expect(
+			wrapper({
+				media: { isAtMediumUp: true },
+				self: { status: 'active', name: 'Jeff Cleft' },
+			})
+		).toMatchSnapshot();
+	});
+
+	it('should match the snapshot for unauthenticated medium screens', () => {
+		expect(
+			wrapper({
+				media: { isAtMediumUp: true },
+				self: { status: 'prereg' },
+			})
+		).toMatchSnapshot();
+	});
+
+	it('should match the snapshot for unauthenticated large screens', () => {
+		expect(
+			wrapper({ media: { isAtLargeUp: true }, self: { status: 'prereg' } })
+		).toMatchSnapshot();
+	});
+
+	it('should match the snapshot for authenticated large screens', () => {
+		expect(
+			wrapper({
+				media: { isAtLargeUp: true },
+				self: { status: 'active', name: 'Jeff Cleft' },
+			})
+		).toMatchSnapshot();
 	});
 });
