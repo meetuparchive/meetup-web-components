b3ee37a98c82f05b86b0131c9d5506da3d0272f2
Lori Hutchek <lori@meetup.com>
2018-03-22 11:04:47 -0400

    Console error :skull: so close (#482)
    * adding bool types for calendar + error list
    
    * adding bool to timeinput

--- src/forms/ErrorList.jsx
+++ src/forms/ErrorList.jsx
@@ -12,29 +12,22 @@ const ERROR_CLASSNAME = 'text--error text--small';
  * Currently supports only a single error due to a
  * limitation with `redux-form`.
  */
-const ErrorList = (props) => {
-	const {
-		errorId,
-		error,
-		...other
-	} = props;
+const ErrorList = props => {
+	const { errorId, error, ...other } = props;
 
 	if (errorId) {
 		other.id = errorId;
 	}
 
 	return (
-		<ul
-			aria-live="assertive"
-			{...other}
-		>
-			{error && <li className={cx(ERROR_CLASSNAME, 'text--small')}>{error}</li> }
+		<ul aria-live="assertive" {...other}>
+			{error && <li className={cx(ERROR_CLASSNAME, 'text--small')}>{error}</li>}
 		</ul>
 	);
 };
 ErrorList.propTypes = {
 	errorId: PropTypes.string,
-	error: PropTypes.string,
+	error: PropTypes.oneOfType([PropTypes.string, PropTypes.element, PropTypes.bool]),
 };
 
 export default ErrorList;
