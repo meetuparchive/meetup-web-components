2cafca30d1f3537e40faf9ba86b76ca944a848bc
Mike Perrotti <mikep@meetup.com>
2018-04-05 13:31:45 -0400

    improve focus handling in modals, visual fixes to stories

--- src/interactive/modal.test.js
+++ src/interactive/modal.test.js
@@ -80,7 +80,7 @@ describe('Modal', () => {
 
 	it('should not render a close button', () => {
 		const modal = TestUtils.renderIntoDocument(
-			<Modal onDismiss={e => {}} closeArea={false}>{content}</Modal>
+			<Modal onDismiss={e => {}} closeArea={false}>{content} <a href="#">focusable element</a></Modal>
 		);
 		const closeButtons = TestUtils.scryRenderedDOMComponentsWithClass(
 			modal,
