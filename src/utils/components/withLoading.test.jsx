a31b8a74b7dedf8a2076dc596f0aa066a728d1f3
Mike Perrotti <mikep@meetup.com>
2018-04-12 16:07:02 -0400

    more tests

--- src/utils/components/withLoading.test.jsx
+++ src/utils/components/withLoading.test.jsx
@@ -6,19 +6,39 @@ import withLoading from './withLoading';
 const dangerousHTML = '<div>Dangerously setting inner HTML</div>';
 const makeDanger = () => ({__html: dangerousHTML});
 
-const testComponentClass = 'testClass';
+// const testComponentClass = 'testClass';
 const TestComponent = (props) => (<div {...props} />);
 const TestComponentWithLoading = withLoading(TestComponent);
 
 describe('withLoading', function() {
-	const testComponentWithLoading = shallow(<TestComponentWithLoading className={testComponentClass} />);
+	const testComponentWithLoading = shallow(<TestComponentWithLoading isLoading />);
+	const testComponentNoChildren = shallow(<TestComponentWithLoading />);
+	const testComponentWithOneChild = shallow(
+		<TestComponentWithLoading>
+			<span>World</span>
+		</TestComponentWithLoading>
+	);
+	const testComponentWithTwoChildren = shallow(
+		<TestComponentWithLoading>
+			<span>Hello</span>
+			<span>World</span>
+		</TestComponentWithLoading>
+	);
 
-	it('matches snapshot', () => {
+	it('matches snapshot with isLoading', () => {
 		expect(testComponentWithLoading).toMatchSnapshot();
 	});
 
-	it('renders a wrapped component', () => {
-		expect(testComponentWithLoading.find(`.${testComponentClass}`).length).toBe(1);
+	it('matches snapshot with 0 children', () => {
+		expect(testComponentNoChildren).toMatchSnapshot();
+	});
+
+	it('matches snapshot with 1 child', () => {
+		expect(testComponentWithOneChild).toMatchSnapshot();
+	});
+
+	it('matches snapshot with 2 children', () => {
+		expect(testComponentWithTwoChildren).toMatchSnapshot();
 	});
 
 	it('can handle dangerouslySetInnerHTML', function() {
