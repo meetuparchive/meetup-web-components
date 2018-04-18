f9ff425484d458b7d7d165555ea2baf0bc9fabe6
Mike Perrotti <mikep@meetup.com>
2018-04-18 13:15:31 -0400

    use ToggleWrapper component for TogglePill

---
+++ src/utils/components/ToggleWrapper.jsx
@@ -0,0 +1,81 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import ConditionalWrap from './ConditionalWrap';
+
+/**
+ * Provides boolean isActive prop to wrapped component.
+ * Prop name can also be customized if need be
+ *
+ * @param {React.element} ToggleComponent - the component to wrap
+ * @returns {React.element}
+ */
+// export default function withToggleControl(WrappedComponent) {
+/**
+ * @module ToggleWrapper
+ */
+class ToggleWrapper extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { isActive: props.isActive };
+		this.toggleActive = this.toggleBool.bind(this);
+		this.onKeyUp = this.onKeyUp.bind(this);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.isActive !== this.props.isActive) {
+			this.toggleActive();
+		}
+	}
+
+	toggleBool(e) {
+		this.setState({ isActive: !this.state.isActive });
+		if (this.props.onToggle) {
+			this.props.onToggle(e);
+		}
+	}
+
+	onKeyUp(e) {
+		const isActivatingButton = [
+			' ', /* space bar */
+			'Enter'
+		].some(key => e.key === key);
+
+		if (isActivatingButton && this.props.type !== 'radio') {
+			this.toggleActive();
+		}
+	}
+
+	render() {
+		return (
+			<ConditionalWrap
+				condition={this.props.type !== 'radio' || this.props.type !== 'checkbox'}
+				wrap={children => (
+					<div
+						role="button"
+						aria-pressed={this.state.isActive}
+						onKeyUp={this.onKeyUp}
+					>
+						{children}
+					</div>
+				)}
+			>
+				{this.props.children({
+					tabIndex: this.props.type !== 'radio' && this.props.tabIndex || "0",
+					isActive: this.state.isActive,
+					toggleActive: this.toggleActive,
+					onKeyUp: this.props.type !== 'radio' && this.onKeyUp
+				})}
+			</ConditionalWrap>
+		);
+	}
+}
+
+ToggleWrapper.defaultProps = {
+	isActive: false
+};
+ToggleWrapper.propTypes = {
+	isActive: PropTypes.bool
+};
+
+export default ToggleWrapper;
