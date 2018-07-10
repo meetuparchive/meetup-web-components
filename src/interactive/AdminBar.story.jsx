35089902b54e946134215ad176c11f342cbc7668
lipenco <mag.lipinska@gmail.com>
2018-07-10 12:00:44 -0400

    add .addWithInfo to story

--- src/interactive/AdminBar.story.jsx
+++ src/interactive/AdminBar.story.jsx
@@ -7,18 +7,18 @@ import AdminBar from './AdminBar';
 
 storiesOf('AdminBar', module)
 	.addDecorator(decorateWithBasics)
-	.addWithInfo('default: isAdmin={false}', 'Component is not rendered if props: isAdmin=false', () => (
-			<AdminBar isAdmin={false} />
-	))
-	.addWithInfo('isAdmin', () => (
-			<AdminBar group={MOCK_GROUP} isAdmin />
-	))
+	.addWithInfo(
+		'default: isAdmin={false}',
+		'Component is not rendered if props: isAdmin=false',
+		() => <AdminBar isAdmin={false} />
+	)
+	.addWithInfo('isAdmin', () => <AdminBar group={MOCK_GROUP} isAdmin />)
 	.addWithInfo('isQL', () => (
-			<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isQL />
+		<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isQL />
 	))
 	.addWithInfo('isProd', () => (
-			<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isProd />
+		<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isProd />
 	))
 	.addWithInfo('isProd and isQL', () => (
-			<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isProd isQL />
+		<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isProd isQL />
 	));
