1d7a0710c9034a87cb9c3a4434dbe395cc38abd0
Michael McGahan <mike.mcgahan@gmail.com>
2018-07-17 12:21:38 +1200

    update snaps with new snapshot serialization of undefined props

---
+++ src/forms/select.story.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Select from './Select';
+import { storiesOf } from '@storybook/react';
+import { decorateWithBasics, decorateWithInfo } from '../utils/decorators';
+
+storiesOf('Select', module)
+	.addDecorator(decorateWithBasics)
+	.addDecorator(decorateWithInfo)
+	.add('default', () => (
+		<Select label="Select a name for your horse" id="horsename" name="horsename">
+			<option value="geoffrey">Geoffrey</option>
+			<option value="drhorse">Doctor Horse, MD Junior</option>
+			<option value="chompyhorse">Mister Chompy</option>
+		</Select>
+	))
+	.add('with default selection using `value`', () => (
+		<Select
+			value="drhorse"
+			label="Select a name for your horse"
+			id="horsename"
+			name="horsename"
+		>
+			<option value="geoffrey">Geoffrey</option>
+			<option value="drhorse">Doctor Horse, MD Junior</option>
+			<option value="chompyhorse">Mister Chompy</option>
+		</Select>
+	))
+	.add('required', () => (
+		<Select
+			label="Select a name for your horse"
+			id="horsename"
+			name="horsename"
+			error="You forgot to name your horse!"
+			required
+		>
+			<option value="geoffrey">Geoffrey</option>
+			<option value="drhorse">Doctor Horse, MD Junior</option>
+			<option value="chompyhorse">Mister Chompy</option>
+		</Select>
+	))
+	.add('with disabled options', () => (
+		<Select label="Select a name for your horse" id="horsename" name="horsename">
+			<option value="geoffrey" disabled>
+				Geoffrey
+			</option>
+			<option value="drhorse">Doctor Horse, MD Junior</option>
+			<option value="chompyhorse">Mister Chompy</option>
+		</Select>
+	))
+	.add('error message', () => (
+		<Select
+			error="I'm a single lady, I'm a single lady"
+			label="Select a name for your horse"
+			id="horsename"
+			name="horsename"
+		>
+			<option value="geoffrey" disabled>
+				Geoffrey
+			</option>
+			<option value="drhorse">Doctor Horse, MD Junior</option>
+			<option value="chompyhorse">Mister Chompy</option>
+		</Select>
+	))
+	.add('with helper text', () => (
+		<Select
+			helperText="Lorem ipsum is simply dummy text"
+			label="Select a name for your horse"
+			id="horsename"
+			name="horsename"
+		>
+			<option value="geoffrey" disabled>
+				Geoffrey
+			</option>
+			<option value="drhorse">Doctor Horse, MD Junior</option>
+			<option value="chompyhorse">Mister Chompy</option>
+		</Select>
+	));
