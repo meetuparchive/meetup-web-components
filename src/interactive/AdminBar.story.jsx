a14a8cd3b9971b8fadf44cbc5c7b38694cfe23d2
lipenco <mag.lipinska@gmail.com>
2018-07-23 11:43:55 -0400

    Display warning about using prodApi is isAdmin is false and nodeEnv is development

--- src/interactive/AdminBar.story.jsx
+++ src/interactive/AdminBar.story.jsx
@@ -8,16 +8,28 @@ import AdminBar from './AdminBar';
 storiesOf('AdminBar', module)
 	.addDecorator(decorateWithBasics)
 	.addDecorator(decorateWithInfo)
-	.add('default: isAdmin={false}', () => <AdminBar isAdmin={false} />, {
-		info: { text: 'Component is not rendered if props: isAdmin=false' },
+	.add('default: isAdmin={false} prod environment', () => (<AdminBar
+	nodeEnv='production'/>), {
+		info: { text: 'Component is not rendered if props: isAdmin=false, nodeEnv=production' },
 	})
-	.add('isAdmin', () => <AdminBar group={MOCK_GROUP} isAdmin />)
+	.add('isAdmin and prod environment', () => (<AdminBar
+	nodeEnv='production'
+	group={MOCK_GROUP} isAdmin />))
 	.add('isQL', () => <AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isQL />)
 	.add('isProdApi', () => (
 		<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isProdApi />
 	))
 	.add('isProdApi and isQL', () => (
 		<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isProdApi isQL />
+	))
+	.add('isQL and prod environment', () => (
+		<AdminBar group={MOCK_GROUP} self={{ id: '' }} isAdmin isQL />
+	))
+	.add('isProdApi and isAdmin=false on dev env', () => (
+		<AdminBar self={{ id: '' }} isProdApi nodeEnv='development' />
+	))
+	.add('isProdApi and isAdmin=false on prod env', () => (
+		<AdminBar self={{ id: '' }} isProdApi nodeEnv='production' />
 	))
 	.add('group is the optional param', () => (
 		<AdminBar self={{ id: '' }} isAdmin isProdApi isQL />
