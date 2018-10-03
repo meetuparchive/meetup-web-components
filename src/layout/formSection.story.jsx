46d14aca2e69948e90f54114c74b6cb90a7d43f9
Malik Browne <mbrowne@meetup.com>
2018-10-03 13:44:49 -0400

    add knobs and types for FormSection

--- src/layout/formSection.story.jsx
+++ src/layout/formSection.story.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { decorateWithInfo } from '../utils/decorators';
+import { withKnobs, boolean, object, text } from '@storybook/addon-knobs';
+
+import { withInfo } from '@storybook/addon-info';
 
 import Bounds from './Bounds';
 import Chunk from './Chunk';
 import FormSection from './FormSection';
 import TextInput from '../forms/TextInput';
+import { C_COOLGRAYMEDIUM } from 'swarm-constants/dist/js/constants';
+import { MEDIA_SIZES } from '../utils/designConstants';
 
 storiesOf('Layout/FormSection', module)
-	.addDecorator(decorateWithInfo)
+	.addDecorator(withInfo)
+	.addDecorator(withKnobs)
 	.addParameters({ info: { propTablesExclude: [Bounds, Chunk, TextInput] } })
 	.add('default', () => (
 		<Bounds>
-			<FormSection>
+			<FormSection
+				withSeparator={boolean('withSeparator', false)}
+				isLoading={boolean('isLoading', false)}
+				className={text('className', '')}
+				loadingProps={object('loadingProps', {
+					color: C_COOLGRAYMEDIUM,
+					size: `${MEDIA_SIZES.l}px`,
+				})}
+			>
 				<Chunk>
 					<h2 className="text--sectionTitle">Part one</h2>
 				</Chunk>
@@ -42,7 +55,15 @@ storiesOf('Layout/FormSection', module)
 	))
 	.add('isLoading', () => (
 		<Bounds>
-			<FormSection isLoading>
+			<FormSection
+				withSeparator={boolean('withSeparator', false)}
+				isLoading={boolean('isLoading', true)}
+				className={text('className', '')}
+				loadingProps={object('loadingProps', {
+					color: C_COOLGRAYMEDIUM,
+					size: `${MEDIA_SIZES.l}px`,
+				})}
+			>
 				<Chunk>
 					<h2 className="text--sectionTitle">Loading form section</h2>
 				</Chunk>
@@ -73,12 +94,14 @@ storiesOf('Layout/FormSection', module)
 	.add('isLoading with loadingProps', () => (
 		<Bounds>
 			<FormSection
-				isLoading
-				loadingProps={{
+				withSeparator={boolean('withSeparator', false)}
+				isLoading={boolean('isLoading', false)}
+				className={text('className', '')}
+				loadingProps={object('loadingProps', {
 					color: 'red',
 					scrimColor: 'rgba(250, 250, 255, 0.8)',
 					size: '64px',
-				}}
+				})}
 			>
 				<Chunk>
 					<h2 className="text--sectionTitle">Loading form section</h2>
