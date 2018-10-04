30a537eb04b961f1e3e0b43902a45bcae61bd8b6
hsbacot <hsbacot@gmail.com>
2018-10-04 12:32:22 -0400

    updated dates to use js-joda LocalDate

--- src/forms/calendarComponent.story.jsx
+++ src/forms/calendarComponent.story.jsx
@@ -3,9 +3,12 @@ import CalendarComponent from './CalendarComponent';
 import { storiesOf } from '@storybook/react';
 import { decorateWithBasics, decorateWithInfo } from '../utils/decorators';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { LocalDate } from 'js-joda';
 
 import Section from '../layout/Section';
 import Chunk from '../layout/Chunk';
+
+const jodaDate = LocalDate.now();
 
 storiesOf('Forms/CalendarComponent', module)
 	.addDecorator(decorateWithBasics)
@@ -17,10 +20,11 @@ storiesOf('Forms/CalendarComponent', module)
 				<CalendarComponent
 					name="event_time"
 					label="Start at"
-					value={new Date()}
+					value={jodaDate}
 					datepickerOptions={{
-						maxDate: boolean('max date today', false) && new Date(),
-						minDate: boolean('min date today', false) && new Date(),
+						maxDate: boolean('max date today', false) && jodaDate,
+						minDate:
+							boolean('min date today', false) && jodaDate.plusWeeks(16),
 					}}
 				/>
 			</Chunk>
@@ -44,7 +48,7 @@ storiesOf('Forms/CalendarComponent', module)
 				<CalendarComponent
 					name="event_time"
 					label="Start at"
-					value={new Date()}
+					value={jodaDate}
 					required
 					requiredText="(required)"
 				/>
@@ -57,7 +61,7 @@ storiesOf('Forms/CalendarComponent', module)
 				<CalendarComponent
 					name="event_time"
 					label="Start at"
-					value={new Date()}
+					value={jodaDate}
 					error="this is an error"
 				/>
 			</div>
@@ -70,57 +74,45 @@ storiesOf('Forms/CalendarComponent', module)
 					name="event_time"
 					label="Start at"
 					helperText="Lorem Ipsum is simply dummy text"
-					value={new Date()}
+					value={jodaDate}
 				/>
 			</div>
 		);
 	})
 	.add('sets a valid date range', () => {
-		const min = new Date(),
-			max = new Date(),
-			now = Date.now();
-		min.setDate(min.getDate() - 2);
-		max.setDate(max.getDate() + 7);
-
-		const opts = {
-			allowInput: true,
-			minDate: min,
-			maxDate: max,
-		};
 		return (
 			<div className="span--50">
 				<CalendarComponent
 					name="event_time"
 					label="From 2 days ago to 1 week from now"
-					value={now}
-					datepickerOptions={opts}
+					value={jodaDate}
+					datepickerOptions={{
+						allowInput: true,
+						minDate: jodaDate.minusDays(2),
+						maxDate: jodaDate.plusDays(7),
+					}}
 				/>
 			</div>
 		);
 	})
 	.add('sets a default date and time', () => {
-		const opts = {
-				allowInput: true,
-				minDate: Date.now(),
-			},
-			date = new Date(3000, 1, 1, 15, 0, 0);
+		const date = LocalDate.of(3000, 1, 1);
 		return (
 			<div className="span--50">
 				<CalendarComponent
 					name="event_time"
 					label="In the year 3000"
 					value={date}
-					datepickerOptions={opts}
+					datepickerOptions={{
+						allowInput: true,
+						minDate: jodaDate,
+					}}
 				/>
 			</div>
 		);
 	})
 	.add('error state', () => {
-		const opts = {
-				allowInput: true,
-				minDate: Date.now(),
-			},
-			date = new Date(3000, 1, 1, 15, 0, 0);
+		const date = LocalDate.of(3000, 1, 1);
 		return (
 			<div className="span--50">
 				<CalendarComponent
@@ -128,7 +120,10 @@ storiesOf('Forms/CalendarComponent', module)
 					label="Start at"
 					value={date}
 					error={'Woops, something went wrong.'}
-					datepickerOptions={opts}
+					datepickerOptions={{
+						allowInput: true,
+						minDate: jodaDate,
+					}}
 				/>
 			</div>
 		);
