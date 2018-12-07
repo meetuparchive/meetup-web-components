0a86a5113985097c07119c53bef431de73d17753
hsbacot <hsbacot@gmail.com>
2018-12-07 13:00:12 -0500

    commenting logic of node-sass importer file resolution

--- util/sassRelativeImporter.js
+++ util/sassRelativeImporter.js
@@ -1,8 +1,12 @@
+// node-sass importer to resolve path names locally of imports from
+// node_modules the same way css-loader does with ~ as a prefix
+// used in build:css to build css from sass
+
 const path = require('path');
 
 module.exports = function(url, prev, done) {
-  if (url[0] === '~') {
-      url = path.resolve('node_modules', url.substr(1));
-  }
-  return { file: url };
+	if (url[0] === '~') {
+		url = path.resolve('node_modules', url.substr(1));
+	}
+	return { file: url };
 };
