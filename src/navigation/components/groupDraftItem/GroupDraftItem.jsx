89d70a12d056379ca9ce1a89c784aa528c4c98b1
Georgi <georgi@softest3-mac.universe.dart.spb>
2019-03-14 17:14:49 +0200

    OG-338: change urlname to editLink

--- src/navigation/components/groupDraftItem/GroupDraftItem.jsx
+++ src/navigation/components/groupDraftItem/GroupDraftItem.jsx
@@ -8,13 +8,13 @@ export const PROFILE_GROUP_DRAFT_LIST_ITEM_CLASS = 'profileDropdown-draft-group'
 export const GroupDraftItem = ({ groupDraft }) => {
 	return (
 		<li
-			key={groupDraft.urlname}
+			key={groupDraft.editLink}
 			className={cx(PROFILE_GROUP_DRAFT_LIST_ITEM_CLASS, 'list-item')}
 		>
 			<p>{groupDraft.name}</p>
 			<div>
 				<span>{groupDraft.status}</span>
-				<a className="draftprofiledropdown" href={groupDraft.urlname}>
+				<a className="draftprofiledropdown" href={groupDraft.editLink}>
 					<strong>{groupDraft.actionTitle}</strong>{' '}
 					<Icon className="text--blue" shape="arrow-right" size="xs" />
 				</a>
@@ -27,9 +27,9 @@ export default GroupDraftItem;
 
 GroupDraftItem.propTypes = {
 	groupDraft: PropTypes.shape({
-		urlname: PropTypes.string,
+		editLink: PropTypes.string,
 		name: PropTypes.string,
-		status: PropTypes.string, // translated 'In progress' string
+		status: PropTypes.string,
 		actionTitle: PropTypes.string,
 	}),
 };
--- src/navigation/nav.story.jsx
+++ src/navigation/nav.story.jsx
@@ -146,7 +146,7 @@ storiesOf('Site Chrome/Nav', module)
 		const profile = {
 			...navItems.profile,
 			groupDraft: {
-				urlname: 'create',
+				editLink: 'create',
 				name: 'Name of the group',
 				status: 'In progress',
 				actionTitle: 'Finish group',
