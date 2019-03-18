0bcc4ec77937ab9a1b2803c4f774950556f1bf21
Sadaf Shahsahebi <sadaf@meetup.com>
2019-03-18 11:46:56 -0700

    fix breaking hscroll component + test

--- src/hscroll.test.jsx
+++ src/hscroll.test.jsx
@@ -37,7 +37,6 @@ describe('Default Hscroll', () => {
 
 describe('Gradient Hscroll', () => {
 	const component = shallow(<Hscroll hasGradient>{listItems}</Hscroll>);
-	const gradientEls = component.find('.hscrollGradientWrap');
 	const MOCK_EVENT = {
 		target: {
 			scrollLeft: '10px',
@@ -45,11 +44,13 @@ describe('Gradient Hscroll', () => {
 	};
 
 	it('has graident class when `gradient` prop is supplied', () => {
+		const gradientEls = component.find('.hscrollGradientWrap');
 		expect(gradientEls.length).toBe(1);
 	});
 
 	it(`adds a the className ${HIDE_GRADIENT_CLASSNAME} when the hscroll has been scrolled`, () => {
 		component.instance().onScroll(MOCK_EVENT);
+		component.update();
 		expect(component.hasClass(HIDE_GRADIENT_CLASSNAME)).toBe(true);
 	});
 });
