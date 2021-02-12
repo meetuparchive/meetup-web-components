7ea2b973a52ed4d9a0e1c2d17747f6e3d5f21d8b
Vladyslav Leibenko <v.s.leibenko@gmail.com>
2021-02-12 16:45:53 +0200

    [176853966] Add search bar to nav header

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -262,7 +262,6 @@ export class Nav extends React.Component {
 			isSearchEnabled &&
 				!media.isAtMediumUp && {
 					shrink: true,
-					icon: search.icon,
 					className: `${search.className} navItem--searchIcon`,
 					onAction: this.onSearchIconClick,
 				},
@@ -305,7 +304,7 @@ export class Nav extends React.Component {
 			},
 			media.isAtMediumUp && !self.is_pro_admin && createMeetupLink,
 			media.isAtMediumUp && experiencesLink,
-			{
+			!isNewNavActive && {
 				shrink: true,
 				linkTo: explore.link,
 				label: explore.label,
@@ -535,6 +534,7 @@ export class Nav extends React.Component {
 										maxWidth: SEARCH_INPUT_MAX_WIDTH,
 										height: SEARCH_INPUT_HEIGHT,
 									}}
+									isNewNavActive={isNewNavActive}
 								/>
 							</FlexItem>
 						)}
@@ -568,6 +568,7 @@ export class Nav extends React.Component {
 								onSearchCallback={onSearchCallback}
 								placeholder={search.placeholder}
 								style={{ height: SEARCH_INPUT_HEIGHT }}
+								isNewNavActive={false}
 							/>
 						</div>
 					)}
