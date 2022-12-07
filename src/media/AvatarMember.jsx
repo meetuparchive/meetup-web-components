4424c3e08881196f4a6ba10700925e857c9f4a21
dsolianyk <60266315+dsolianyk@users.noreply.github.com>
2022-12-07 19:59:52 +0200

    add badge to avatar (#825)
    Co-authored-by: Dima Solyanik <dsolyanik@meetup.com>

--- src/media/AvatarMember.jsx
+++ src/media/AvatarMember.jsx
@@ -9,8 +9,16 @@ export const AVATAR_PERSON_ORG_CLASS = 'avatar--org';
 export const AVATAR_PERSON_FB_CLASS = 'avatar--fbFriend';
 export const AVATAR_PERSON_NOPHOTO_CLASS = 'avatar--noPhoto';
 const AVATAR_ICON_BADGE_CLASS = 'svg--avatarBadge';
+import organizerBadge from '../../assets/svg/organizer-badge.svg';
+import memberPlusBadge from '../../assets/svg/member-plus-badge.svg';
+import hostBadge from '../../assets/svg/host-badge.svg';
 
 const AVATAR_IMAGE_SIZE_LIST = ['big', 'default'];
+const BADGES = {
+	ORGANIZER: 'organizer',
+	MEMBER_PLUS: 'memberPlus',
+	HOST: 'host',
+};
 
 export const getPhoto = (photo, size) => {
 	if (!photo) {
@@ -32,13 +40,34 @@ export const getPhotoSize = (imageSize, isBig) => {
 	return isBig ? 'big' : 'default';
 };
 
+const getBadge = type => {
+	switch (type) {
+		case BADGES.ORGANIZER:
+			return <img className={AVATAR_ICON_BADGE_CLASS} src={organizerBadge} />;
+		case BADGES.MEMBER_PLUS:
+			return <img className={AVATAR_ICON_BADGE_CLASS} src={memberPlusBadge} />;
+		case BADGES.HOST:
+			return <img className={AVATAR_ICON_BADGE_CLASS} src={hostBadge} />;
+		default:
+			return null;
+	}
+};
+
 /**
  * An avatar for a member - just supply a member
  * @module AvatarMember
  */
 class AvatarMember extends React.PureComponent {
 	render() {
-		const { member, org, fbFriend, className, imageSize, ...other } = this.props;
+		const {
+			member,
+			org,
+			fbFriend,
+			className,
+			imageSize,
+			badgeType,
+			...other
+		} = this.props;
 		const { big, large, xxlarge } = other;
 
 		const photoSize = getPhotoSize(imageSize, big || large || xxlarge);
@@ -48,7 +77,7 @@ class AvatarMember extends React.PureComponent {
 		const classNames = cx(
 			AVATAR_PERSON_CLASS,
 			{
-				[AVATAR_PERSON_ORG_CLASS]: org,
+				[AVATAR_PERSON_ORG_CLASS]: org || badgeType,
 				[AVATAR_PERSON_FB_CLASS]: fbFriend,
 				[AVATAR_PERSON_NOPHOTO_CLASS]: showNoPhoto,
 			},
@@ -84,6 +113,7 @@ class AvatarMember extends React.PureComponent {
 				{org && (
 					<Icon className={AVATAR_ICON_BADGE_CLASS} shape="badge" size="m" />
 				)}
+				{badgeType && getBadge(badgeType)}
 			</Avatar>
 		);
 	}
@@ -113,6 +143,11 @@ AvatarMember.propTypes = {
 
 	/** Image size */
 	imageSize: PropTypes.oneOf(AVATAR_IMAGE_SIZE_LIST),
+
+	/** Badge type */
+	badgeType: PropTypes.oneOf(Object.values(BADGES)),
 };
+
+AvatarMember.BADGES = BADGES;
 
 export default AvatarMember;
