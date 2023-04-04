e943eafea91e9deb3be3ea89d0da9c71c1f59d0e
hulakantony <hulak.antony1992@gmail.com>
2023-04-04 13:37:20 +0300

    update accessibility issue

--- src/interactive/Tooltip.jsx
+++ src/interactive/Tooltip.jsx
@@ -178,6 +178,7 @@ class Tooltip extends React.PureComponent {
 											}
 											reset
 											aria-label="Close"
+											role="button"
 										/>
 									)}
 									{content}
