07ef3e9f068452a02af8b07965b8da8a8fd92fe0
Oneil <owilliams@meetup.com>
2023-04-18 10:21:08 -0500

    update story

--- src/media/appBadges.story.jsx
+++ src/media/appBadges.story.jsx
@@ -7,4 +7,8 @@ import AppBadges from './AppBadges';
 storiesOf('Media/AppBadges', module)
 	.addDecorator(decorateWithBasics)
 	.addDecorator(decorateWithInfo)
-	.add('default', () => <AppBadges language="en" />);
+	.add('default', () => <AppBadges language="en" />)
+	.add('only Google Play button', () => (
+		<AppBadges language="en" isAndroidPhone isMobile />
+	))
+	.add('only App Store button', () => <AppBadges language="de" isIosPhone isMobile />);
